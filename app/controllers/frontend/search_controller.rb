4e6e7b57d3ddfd48ae1aba8acae7fd07905cd9ad
Moisés Donoso Salinas <moisesdonoso@gmail.com>
2018-12-13 17:05:19 -0300

    format search data

--- app/controllers/frontend/search_controller.rb
+++ app/controllers/frontend/search_controller.rb
@@ -3,6 +3,9 @@ module Frontend
 
     def search
       search = params[:term].present? ? params[:term] : nil
+      users = []
+      posts = []
+      users = []
       if search
         result = Searchkick.search search, index_name: [General::User, News::Post, General::Menu]
         @users = result.with_hit.map{|a| a[0] if a[1]["_type"] == "general/user"}.compact
@@ -17,13 +20,23 @@ module Frontend
 
     def search_vue
       data = []
+      users = []
       search = params[:term].present? ? params[:term] : nil
       if search
         result = Searchkick.search search, index_name: [General::User, News::Post, General::Menu]
-        @users = result.with_hit.map{|a| a[0] if a[1]["_type"] == "general/user"}.compact
+        result.with_hit.map{|a| a[0] if a[1]["_type"] == "general/user"}.compact.each do |user|
+          users << {
+            name: user.name,
+            last_name: user.last_name,
+            email: user.email,
+            annexed: user.annexed,
+            birthday: user.birthday,
+            image: @ip.to_s + Rails.application.routes.url_helpers.rails_blob_path(user.image, only_path: true)
+          }
+        end
         @posts = result.with_hit.map{|a| a[0] if a[1]["_type"] == "news/post"}.compact
         @menus = result.with_hit.map{|a| a[0] if a[1]["_type"] == "general/menu"}.compact
-        data << [@users, @posts, @menus]
+        data << [users, @posts, @menus]
       else
         data << { message: 'Ingresar parametro de busqueda' }
       end
