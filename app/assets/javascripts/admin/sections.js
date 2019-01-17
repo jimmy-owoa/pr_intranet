dd7a8fc4737191d06dc11263fe735a6603b66acc
Moisés Donoso Salinas <moisesdonoso@gmail.com>
2019-01-17 15:56:22 -0300

    editing section form

--- app/assets/javascripts/admin/sections.js
+++ app/assets/javascripts/admin/sections.js
@@ -1,6 +1,13 @@
 $(document).on('turbolinks:load', function () {
   $("#section_url").select2({
     theme: "bootstrap",
-    width: '100%'
+    width: '100%',
+    minimumInputLength: 1,
+    formatInputTooShort: "",
+    language: {
+      inputTooShort: function () {
+        return 'Ingresar al menos 1 caracter';
+      }
+    }
   });
 })
