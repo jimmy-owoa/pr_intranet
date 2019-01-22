a798091aacd90d362c725b5d920790a756dbd882
Moisés Donoso Salinas <moisesdonoso@gmail.com>
2019-01-22 12:13:08 -0300

    fix searchkick for word search

--- app/controllers/admin/search_controller.rb
+++ app/controllers/admin/search_controller.rb
@@ -3,7 +3,7 @@ class Admin::SearchController < ApplicationController
     def search
       search = params[:term].present? ? params[:term] : nil
       if search
-        result = Searchkick.search search, index_name: [General::User, News::Post, General::Menu]
+        result = Searchkick.search search, index_name: [General::User, News::Post, General::Menu], match: :word
         @users = result.with_hit.map{|a| a[0] if a[1]["_type"] == "general/user"}.compact
         @posts = result.with_hit.map{|a| a[0] if a[1]["_type"] == "news/post"}.compact
         @menus = result.with_hit.map{|a| a[0] if a[1]["_type"] == "general/menu"}.compact
