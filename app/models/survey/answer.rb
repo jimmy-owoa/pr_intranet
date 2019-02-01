d6999706407f00a30d1937e3ea92c905572a6677
Moisés Donoso Salinas <moisesdonoso@gmail.com>
2019-02-01 12:42:11 -0300

    fix callback

--- app/models/survey/answer.rb
+++ app/models/survey/answer.rb
@@ -2,12 +2,12 @@ class Survey::Answer < ApplicationRecord
   belongs_to :option, optional: true
   belongs_to :question, optional: true
   belongs_to :user, class_name: 'General::User', optional: true
-  before_save :set_question_id
+  before_save :set_question_id, :already_answered
 
-  validate :already_answered
+  # validate :already_answered
 
   def already_answered
-    Survey::Answer.where(user_id: user_id, question_id: question_id, option_id: option_id).blank?
+    throw :abort if Survey::Answer.exists?(user_id: user_id, question_id: question_id, option_id: option_id)
   end
 
   def set_question_id
