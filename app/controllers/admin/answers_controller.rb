4548ec7146ec203d4eabf48edce5bb725d720e96
Moisés Donoso Salinas <moisesdonoso@gmail.com>
2019-02-01 11:59:02 -0300

    fixing set survey

--- app/controllers/admin/answers_controller.rb
+++ app/controllers/admin/answers_controller.rb
@@ -4,6 +4,7 @@ class Admin::AnswersController < ApplicationController
 
   def index
     @answers = Survey::Answer.all
+    @surveys = Survey::Survey.all
   end
 
   def show
@@ -12,8 +13,8 @@ class Admin::AnswersController < ApplicationController
   private
   def set_survey
     @answer = Survey::Answer.find(params[:id])
-    @option = Survey::Option.find(@answer.option_id)
-    @question = @option.question
+    @question = Survey::Question.find(@answer.question_id)
     @survey = Survey::Survey.find(@question.survey_id)
+    @option = Survey::Option.find(@answer.option_id) if @answer.option_id.present?
   end
 end
