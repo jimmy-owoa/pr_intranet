e725ad128085bc8f18695a911ebf66e88f771cf4
Samuel Infante <sinfante@redvel.cl>
2019-02-05 18:54:32 -0300

    encuestas cambios varios

--- app/models/survey/survey.rb
+++ app/models/survey/survey.rb
@@ -1,5 +1,42 @@
 class Survey::Survey < ApplicationRecord
-    has_many :questions, dependent: :destroy
+  has_many :questions, dependent: :destroy
 
-    accepts_nested_attributes_for :questions, allow_destroy: true, reject_if: proc { |att| att['title'].blank?}
+  accepts_nested_attributes_for :questions, allow_destroy: true, reject_if: proc { |att| att['title'].blank? }
+
+  before_save :unique_slug, :set_survey_type
+
+  SURVEY_TYPES = [['Encuesta','survey'],['Formulario','form']]
+
+  private
+  def set_survey_type
+    self.survey_type = self.survey_type ||= 'survey'
+  end
+
+  def unique_slug
+    self.slug = if self.slug.nil?
+        set_slug(self.name.parameterize)
+      else
+        set_slug(self.slug)
+      end
+  end
+
+  def set_slug(val)
+    survey_by_slug = Survey::Survey.find_by(slug: val)
+    if survey_by_slug.present? && survey_by_slug != self
+      random_number = rand(1000..9999)
+      slug_split = val.split('-')
+      if slug_split[-1].match? /^[0-9]+$/
+        if slug_split.count > 1
+          temp = slug_split[0..-2].join('-')
+        else
+          temp = slug_split[0]
+        end
+        set_slug(temp + '-' + random_number.to_s)
+      else
+        set_slug(val + '-' + random_number.to_s)
+      end
+    else
+      val
+    end
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_31_194233) do
+ActiveRecord::Schema.define(version: 2019_02_05_191442) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -358,6 +358,9 @@ ActiveRecord::Schema.define(version: 2019_01_31_194233) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "survey_type"
+    t.string "slug"
+    t.index ["slug"], name: "index_survey_surveys_on_slug", unique: true
   end
 
   add_foreign_key "general_term_relationships", "general_terms", column: "term_id"
