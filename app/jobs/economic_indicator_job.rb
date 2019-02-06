d3cb3c7d1892ea922a89e5d5a335c0a2c11c2b72
Jeremías <jeremias@exaconsultores.cl>
2019-02-06 17:09:32 -0300

    economic indicators finish

--- app/jobs/economic_indicator_job.rb
+++ app/jobs/economic_indicator_job.rb
@@ -2,12 +2,13 @@ class EconomicIndicatorJob < ApplicationJob
   queue_as :economic_indicator
 
   def perform(*args)
+
     today =  Date.today.strftime("%d/%m/%Y")
-    dolar = IndicatorService.perform[:money]['dolar']
-    euro = IndicatorService.perform[:money]['euro']
-    uf = IndicatorService.perform[:indicator]['uf']
-    utm = IndicatorService.perform[:indicator]['utm']
-    ipc = IndicatorService.perform[:indicator]['ipc']
+    dolar = IndicatorService.perform[:dolar]
+    euro = IndicatorService.perform[:euro]
+    uf = IndicatorService.perform[:uf]
+    utm = IndicatorService.perform[:utm]
+    ipc = IndicatorService.perform[:ipc]
     General::EconomicIndicator.create(date: today, economic_indicator_type_id: 1, value: dolar)
     General::EconomicIndicator.create(date: today, economic_indicator_type_id: 2, value: euro)
     General::EconomicIndicator.create(date: today, economic_indicator_type_id: 3, value: uf)
