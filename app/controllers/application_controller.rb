4ce4f23e253ab4dc4748b90d2b144f442e81c39e
Jeremías <jeremias@exaconsultores.cl>
2019-02-06 17:29:12 -0300

    ip into credentials

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,8 @@ class ApplicationController < ActionController::Base
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
   def set_ip
-    @ip = "http://192.168.0.51:3000" 
+    @ip = Rails.application.credentials.develop # Develop ip
+    # @ip = Rails.application.credentials.production # Production ip
   end
 
   def after_sign_in_path_for(user)
