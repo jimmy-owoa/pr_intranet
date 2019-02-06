71ebdbd080bb5b037120098358c922c117d7c5d9
Moisés Donoso Salinas <moisesdonoso@gmail.com>
2019-02-06 16:10:36 -0300

    fix simple, if change update option

--- app/controllers/frontend/answers_controller.rb
+++ app/controllers/frontend/answers_controller.rb
@@ -26,6 +26,22 @@ class Frontend::AnswersController < ApplicationController
       end
     end
 
+    def answers_options_save_from_vue 
+      answer = Survey::Answer.where(user_id: params[:user_id], question_id: params[:question_id]).try(:first) || Survey::Answer.new(user_id: params[:user_id], question_id: params[:question_id])
+      if answer.present?
+        answer.update(option_id: params[:option_id])
+      else
+        respond_to do |format|
+          if answer.save
+            format.json
+          else
+            format.html {render :new}
+            format.json {render json: answer.errors, status: :unprocessable_entity}
+          end
+        end
+      end
+    end    
+
     def answers_save_from_vue
       answer = Survey::Answer.where(user_id: params[:user_id], question_id: params[:question_id]).try(:first) || Survey::Answer.new(user_id: params[:user_id], question_id: params[:question_id])
       answer.answer_variable = params[:answer_variable]
@@ -38,21 +54,7 @@ class Frontend::AnswersController < ApplicationController
         end
 
       end
-    end
-    
-    def answers_options_save_from_vue 
-      answer = Survey::Answer.where(user_id: params[:user_id], question_id: params[:question_id]).try(:first) || Survey::Answer.new(user_id: params[:user_id], question_id: params[:question_id])
-      answer.option_id = params[:option_id]
-      respond_to do |format|
-        if answer.save
-          format.json
-        else
-          format.html {render :new}
-          format.json {render json: answer.errors, status: :unprocessable_entity}
-        end
-
-      end
-    end    
+    end 
   
     def update
       respond_to do |format|
