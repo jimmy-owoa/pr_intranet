4c1381a49c0ba1949673e918be6b82482a1b2b44
Moisés Donoso Salinas <moisesdonoso@gmail.com>
2019-02-13 15:14:35 -0300

    seed for santoral

--- db/schema.rb
+++ db/schema.rb
@@ -174,6 +174,13 @@ ActiveRecord::Schema.define(version: 2019_02_08_191534) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "general_santorals", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "santoral_day"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "general_sections", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title"
     t.text "description"
