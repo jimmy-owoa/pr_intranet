adbab0e080010435343377c040170e368927bfdd
Moisés Donoso Salinas <moisesdonoso@gmail.com>
2019-02-27 15:54:01 -0300

    fix menu and change type of post

--- app/models/news/post.rb
+++ app/models/news/post.rb
@@ -21,10 +21,7 @@ class News::Post < ApplicationRecord
   STATUS = ['Publicado','Borrador', 'Programado']
   VISIBILITY = ['Público', 'Privada']
   FORMAT = {0 => 'Estilo 1', 1 => 'Estilo 2', 2 => 'Estilo 3'}
-  TYPE = [
-    'Noticia',
-    'Campaña'
-  ]
+  TYPE = ['Corporativas', 'Miscelaneos', 'Conociéndonos']
   PERMISSION = [
     'Incluyente',
     'Excluyente'
