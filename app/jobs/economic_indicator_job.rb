f28ee5770bf303ccae3aa023404087d3ae4deb13
Moisés Donoso Salinas <moisesdonoso@gmail.com>
2019-03-06 11:47:56 -0300

    db and utm for uf

--- app/jobs/economic_indicator_job.rb
+++ app/jobs/economic_indicator_job.rb
@@ -10,11 +10,11 @@ class EconomicIndicatorJob < ApplicationJob
     utm = data[:utm]
     ipc = data[:ipc]
     ipsa = data[:ipsa]
-    uf_last = General::EconomicIndicator.where(economic_indicator_type_id: 3).last
+    utm_last = General::EconomicIndicator.where(economic_indicator_type_id: 4).last
     General::EconomicIndicator.where(date: today, economic_indicator_type_id: 1, value: dolar).first_or_create
     General::EconomicIndicator.where(date: today, economic_indicator_type_id: 2, value: euro).first_or_create
-    General::EconomicIndicator.where(date: today, economic_indicator_type_id: 3, value: uf).first_or_create if uf_last.date.strftime('%m/%Y') != today.strftime('%m/%Y')
-    General::EconomicIndicator.where(date: today, economic_indicator_type_id: 4, value: utm).first_or_create
+    General::EconomicIndicator.where(date: today, economic_indicator_type_id: 3, value: uf).first_or_create 
+    General::EconomicIndicator.where(date: today, economic_indicator_type_id: 4, value: utm).first_or_create if uf_last.date.strftime('%m/%Y') != today.strftime('%m/%Y')
     General::EconomicIndicator.where(date: today, economic_indicator_type_id: 5, value: ipc).first_or_create
     General::EconomicIndicator.where(date: today, economic_indicator_type_id: 6, value: ipsa).first_or_create
   end
