e0fbcad686d6d8329d647b30f5e28b2e43e7ceb6
Moisés Donoso Salinas <moisesdonoso@gmail.com>
2019-03-08 16:27:57 -0300

    adding terms to models

--- app/models/general/benefit.rb
+++ app/models/general/benefit.rb
@@ -1,3 +1,8 @@
 class General::Benefit < ApplicationRecord
-    has_one_attached :image
+  has_one_attached :image
+  has_many :benefit_term_relationships, -> {where(object_type: 'General::Benefit')},
+            class_name: 'General::TermRelationship', foreign_key: :object_id, inverse_of: :benefit
+  has_many :terms, through: :benefit_term_relationships  
+
+  accepts_nested_attributes_for :terms
 end
