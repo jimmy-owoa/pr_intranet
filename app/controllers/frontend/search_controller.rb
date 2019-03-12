bfce34718d3489d2165e7a56ba9c4cf8ae5b3219
Moisés Donoso Salinas <moisesdonoso@gmail.com>
2019-03-12 10:59:26 -0300

    home welcome and birthday

--- app/controllers/frontend/search_controller.rb
+++ app/controllers/frontend/search_controller.rb
@@ -9,7 +9,7 @@ module Frontend
       posts = []
       search = params[:term].present? ? params[:term] : nil
       if search
-        result = Searchkick.search search, index_name: [General::User, News::Post, General::Menu], match: :word
+        result = Searchkick.search search, index_name: [General::User, News::Post, General::Menu], match: :word_middle
         result.with_hit.map{|a| a[0] if a[1]["_type"] == "general/user"}.compact.each do |user|
           users << {
             id: user.id,
