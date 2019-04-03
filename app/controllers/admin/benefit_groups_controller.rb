55f4817f5d9df604a36d9af52ee8c18188641441
Moisés Donoso Salinas <moisesdonoso@gmail.com>
2019-04-03 16:51:39 -0300

    add benefit group index

--- app/controllers/admin/benefit_groups_controller.rb
+++ app/controllers/admin/benefit_groups_controller.rb
@@ -3,7 +3,7 @@ module Admin
     before_action :set_benefit_group, only: [:show, :edit, :update, :destroy]
 
     def index
-      @benefit_groups = General::BenefitGroup.order(id: :desc).all
+      @benefit_groups = General::BenefitGroup.paginate(:page => params[:page], :per_page => 10)
     end
 
     def show
