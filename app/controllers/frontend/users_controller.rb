fcb96cc19a16e654d85eea50655b2f5e56392837
Jeremías <jeremias@exaconsultores.cl>
2019-04-04 12:01:07 -0300

    fix user controller

--- app/controllers/frontend/users_controller.rb
+++ app/controllers/frontend/users_controller.rb
@@ -86,12 +86,13 @@ module Frontend
       id = params[:id].present? ? params[:id] : nil
       @user = General::User.find(id)    
       @location = General::Location.find(@user.location_id).name
+      address = General::Location.where(name: @user.address).last.id if @user.address.present?
       @today =  Date.today.strftime("%d/%m/%Y")
       @tomorrow = l(Date.today + 1, format: '%A')
       @tomorrow_1 = l(Date.today + 2, format: '%A')
       @tomorrow_2 = l(Date.today + 3, format: '%A')
       @tomorrow_3 = l(Date.today + 4, format: '%A')
-      @weather = @user.address.present? ? General::WeatherInformation.where(name: @user.address).last : General::WeatherInformation.where(location_id: @user.location_id).last
+      @weather = @user.address.present? ? General::WeatherInformation.where(location_id: address).last : General::WeatherInformation.where(location_id: @user.location_id).last
       @nickname = nickname(@user.name)
       data_childrens = []
       data_siblings = []
