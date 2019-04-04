43216d7cb81b2d4d552226d382e3728f4aac6959
Jeremías <jeremias@exaconsultores.cl>
2019-04-04 15:32:17 -0300

    has many

--- app/models/general/benefit_group.rb
+++ app/models/general/benefit_group.rb
@@ -1,5 +1,5 @@
 class General::BenefitGroup < ApplicationRecord
   #relations
-  has_many :general_benefits, class_name: 'General::Benefit', foreign_key: :benefit_group_id
-  has_many :general_users, class_name: 'General::User', foreign_key: :benefit_group_id
+  has_many :benefits, class_name: 'General::Benefit', foreign_key: :benefit_group_id
+  has_many :users, class_name: 'General::User', foreign_key: :benefit_group_id
 end
