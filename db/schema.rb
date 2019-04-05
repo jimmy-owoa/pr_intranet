8861aea07abf2f451c5060a0b2947b6de9c8beff
Moisés Donoso Salinas <moisesdonoso@gmail.com>
2019-04-05 16:48:21 -0300

    benefit tinmymce content

--- db/schema.rb
+++ db/schema.rb
@@ -120,7 +120,6 @@ ActiveRecord::Schema.define(version: 2019_04_04_203349) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "benefit_group_id"
   end
 
   create_table "general_daily_informations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -299,7 +298,6 @@ ActiveRecord::Schema.define(version: 2019_04_04_203349) do
   end
 
   create_table "general_weather_informations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "location_id"
     t.date "date"
     t.string "max_temp"
     t.string "min_temp"
@@ -320,6 +318,7 @@ ActiveRecord::Schema.define(version: 2019_04_04_203349) do
     t.string "aaa_tomorrow_icon"
     t.string "aaa_tomorrow_max"
     t.string "aaa_tomorrow_min"
+    t.integer "location_id"
   end
 
   create_table "marketplace_products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
