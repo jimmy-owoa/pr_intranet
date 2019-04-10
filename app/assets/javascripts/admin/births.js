825c3e52a7a902e8dcddf55518415d1ab8159e53
Moisés Donoso Salinas <moisesdonoso@gmail.com>
2019-04-10 10:51:14 -0400

    birth modals

--- app/assets/javascripts/admin/births.js
+++ app/assets/javascripts/admin/births.js
@@ -18,4 +18,20 @@ $(document).on('turbolinks:load', function () {
       }
     }).done(function (e) {});
   });
+
+  $(".attach_birth").on("change", function (e) {
+    var id;
+    id = $(this).data("id");
+    return $.ajax({
+      headers: {
+        'X-CSRF-Token': $('meta[name="csrf-token"]').attr('content')
+      },
+      url: "/admin/births/" + id + ".json",
+      type: 'PUT',
+      data: {
+        approved: $(this).is(":checked")
+      }
+    }).done(function (e) {});
+  });
+
 });
