a80345e81f93d7217d76219951028dc023141f3c
Moisés Donoso Salinas <moisesdonoso@gmail.com>
2019-04-11 15:39:39 -0400

    fixing show

--- app/views/admin/posts/datos.rb
+++ app/views/admin/posts/datos.rb
@@ -22,3 +22,74 @@ extract:  nil,
 former_id:  nil,
 migrated_id:  nil,
 migrated_image_filename:  nil
+
+
+
+
+
+
+
+<div class="row title_form">
+<div class="col-sm-12">
+  <div class="card">
+    <div class="card-header">
+      <div class="col-md-12">
+        <div class="col-md-8">
+          <% unless @post.main_image == nil %>
+            <%= image_tag @post.main_image.large, class:'img-fluid'%>
+          <% end %>
+          <div>
+            <%= get_post_status(@post) %>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <%= raw @post.content %>
+        </div>
+    </div>
+    <div class="card-body">
+      <p class="card-text">
+        </br>
+      </p>
+      <p class="card-text">
+        <p><strong> Tags incluyentes: </strong></p>
+        <%= @post.terms.inclusive_tags.map(&:name).join(', ') %>
+        <p><strong> Tags excluyentes: </strong></p>
+        <%= @post.terms.excluding_tags.map(&:name).join(', ') %>
+      </p>
+      <div align="center">
+        <% if @post.galleries.present? %>
+          <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".gallery-modal-lg" id='show_gallery'>Ver Galería</button>
+        <% end %>
+      </div>
+      <div class="col-md-12">
+        <div class="modal fade gallery-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+          <div class="modal-dialog modal-lg modal-dialog-centered">
+            <div class="modal-content">
+              <div class="row text-center" >
+                <% @post.galleries.each do |gal| %>
+                  <figure>
+                    <figcaption class='text-center'><%= gal.name%></figcaption>
+                    <% gal.attachments.each do |image| %>
+                      <%= image_tag(image.attachment.attachment.variant(resize: '400x300>'), style: 'padding: 6px;') %>
+                    <% end %>
+                <% end %>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <br>
+      <p class="card-text">
+        <b>Última actualización:</b> <%= l(@post.updated_at, format: :long2) %>
+      </p>
+    </div>
+  </div>
+</div>
+</div>
+
+<div align="center">
+<%= link_to t('.back', :default => t("helpers.links.back")), admin_posts_path, :class => 'btn btn-primary'  %>
+<%= link_to t('.edit', :default => t("helpers.links.edit")), edit_admin_post_path(@post), :class => 'btn btn-primary' %>
+<%= link_to 'Deshabilitar', admin_post_path(@post),:method => 'delete',:data => { :confirm => t('.confirm', :default => t("helpers.links.confirm", :default => '¿Estás seguro?')) },:class => 'btn btn-danger' %>
+</div>
+</div>
