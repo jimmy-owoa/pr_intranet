3361619f55d4789e6cd00fd0fe57f60632bc6372
Jeremías <jeremias@exaconsultores.cl>
2019-05-03 11:07:48 -0400

    intento numero 3

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -97,7 +97,7 @@ module Admin
 
     def post_params
       params.require(:post).permit(:title, :slug, :content, :status,
-      :main_image_id, :terms, :post_parent_id, :visibility, :post_class, :post_order, 
+      :main_image_id, :main_image, :terms, :post_parent_id, :visibility, :post_class, :post_order, 
       :published_at, :user_id, :post_type, :format, :permission, :important, :extract,
       gallery_ids: [], term_ids: [], terms_names: [])
     end
