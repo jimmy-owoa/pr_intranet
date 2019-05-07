a628dcf5cfdd0bdca9366e18a7407c74d72dc4a4
Jeremías <jeremias@exaconsultores.cl>
2019-05-07 15:34:36 -0400

    init

--- app/models/general/benefit.rb
+++ app/models/general/benefit.rb
@@ -4,9 +4,10 @@ class General::Benefit < ApplicationRecord
             class_name: 'General::TermRelationship', foreign_key: :object_id, inverse_of: :benefit
   has_many :terms, through: :benefit_term_relationships
   has_many :benefit_group_relationships, class_name: 'General::BenefitGroupRelationship', foreign_key: :benefit_id
-
   has_many :benefit_groups, through: :benefit_group_relationships
 
+  belongs_to :benefit_type 
+  
   accepts_nested_attributes_for :terms
   accepts_nested_attributes_for :benefit_groups
 end
