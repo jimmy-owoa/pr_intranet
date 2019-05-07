d9b8f2bf6917ea0e5289bec3875c90a8955cd6b2
Moisés Donoso Salinas <moisesdonoso@gmail.com>
2019-05-07 13:44:33 -0400

    copyclipboard route media

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -48,6 +48,33 @@
 
 // #clipboard
 $(document).ready(function() {
+  $(".clipboard-btn").tooltip({
+    trigger: "click",
+    placement: "bottom"
+  });
+
+  function setTooltip(btn, message) {
+    $(btn)
+      .tooltip("show")
+      .attr("data-original-title", message)
+      .tooltip("show");
+  }
+
+  function hideTooltip(btn) {
+    setTimeout(function() {
+      $(btn).tooltip("hide");
+    }, 1000);
+  }
+
   var clipboard = new Clipboard(".clipboard-btn");
-  console.log(clipboard);
+
+  clipboard.on("success", function(e) {
+    setTooltip(e.trigger, "¡Copiado!");
+    hideTooltip(e.trigger);
+  });
+
+  clipboard.on("error", function(e) {
+    setTooltip(e.trigger, "Failed!");
+    hideTooltip(e.trigger);
+  });
 });
