143bd027853b2efc65be49dd2ee445aa379c40b9
Moisés Donoso Salinas <moisesdonoso@gmail.com>
2019-05-07 14:09:40 -0400

    fix index attachments

--- app/assets/javascripts/admin/attachments.js
+++ app/assets/javascripts/admin/attachments.js
@@ -1,4 +1,34 @@
 $(document).on("turbolinks:load", function() {
+  $(".clipboard-btn").tooltip({
+    trigger: "click",
+    placement: "bottom"
+  });
+
+  function setTooltip(btn, message) {
+    $(btn)
+      .tooltip("show")
+      .attr("data-original-title", message)
+      .tooltip("show");
+  }
+
+  function hideTooltip(btn) {
+    setTimeout(function() {
+      $(btn).tooltip("hide");
+    }, 1000);
+  }
+
+  var clipboard = new Clipboard(".clipboard-btn");
+
+  clipboard.on("success", function(e) {
+    setTooltip(e.trigger, "¡Copiado!");
+    hideTooltip(e.trigger);
+  });
+
+  clipboard.on("error", function(e) {
+    setTooltip(e.trigger, "Failed!");
+    hideTooltip(e.trigger);
+  });
+
   $(".image-picker").imagepicker();
   $("#excluding_tags").select2({
     tags: false,
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -45,36 +45,3 @@
 //= require jquery.minicolors.simple_form
 //= require ahoy
 //= require_self
-
-// #clipboard
-$(document).ready(function() {
-  $(".clipboard-btn").tooltip({
-    trigger: "click",
-    placement: "bottom"
-  });
-
-  function setTooltip(btn, message) {
-    $(btn)
-      .tooltip("show")
-      .attr("data-original-title", message)
-      .tooltip("show");
-  }
-
-  function hideTooltip(btn) {
-    setTimeout(function() {
-      $(btn).tooltip("hide");
-    }, 1000);
-  }
-
-  var clipboard = new Clipboard(".clipboard-btn");
-
-  clipboard.on("success", function(e) {
-    setTooltip(e.trigger, "¡Copiado!");
-    hideTooltip(e.trigger);
-  });
-
-  clipboard.on("error", function(e) {
-    setTooltip(e.trigger, "Failed!");
-    hideTooltip(e.trigger);
-  });
-});
