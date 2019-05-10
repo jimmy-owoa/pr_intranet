5a8478d068d3b28d4314c27600dbc2c6b2b9ff8d
Moisés Donoso Salinas <moisesdonoso@gmail.com>
2019-05-10 10:28:28 -0400

    images

--- app/controllers/frontend/posts_controller.rb
+++ app/controllers/frontend/posts_controller.rb
@@ -104,6 +104,7 @@ module Frontend
     if Rails.env.development?
       content = content.gsub("<source src=\"../..", '<video src="http://localhost:3000')
       content = content.gsub("<img src=\"../..", '<img src="http://localhost:3000')
+      content = content.gsub("<img src=\"rails/", '<img src="http://localhost:3000/rails/')
     else
       content = content.gsub("<source src=\"../..", '<video src="http://intranet-security-qa-v1.s3-website.us-east-2.amazonaws.com')
       content = content.gsub("<img src=\"../..", '<img src="http://intranet-security-qa-v1.s3-website.us-east-2.amazonaws.com')
