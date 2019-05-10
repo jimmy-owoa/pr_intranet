535d293b97fb7cfd9036e6a643b9ae7b25d5c128
Moisés Donoso Salinas <moisesdonoso@gmail.com>
2019-05-10 14:42:08 -0400

    fix

--- app/controllers/frontend/users_controller.rb
+++ app/controllers/frontend/users_controller.rb
@@ -79,7 +79,7 @@ module Frontend
         format.js
       end
     end
-    
+
     def parents_data
       id = params[:id].present? ? params[:id] : nil
       user = General::User.find(id)
@@ -139,8 +139,7 @@ module Frontend
       data_user = []
       id = params[:full_legal_number].present? ? params[:full_legal_number] : nil
       rut= params[:full_legal_number]
-      
-      @user = General::User.find(3453)
+      @user= General::User.find_by_legal_number(rut[0...-1])
       @nickname = nickname(@user.name)
       data_benefits = []
       data_products = []
@@ -213,7 +212,7 @@ module Frontend
         id: @user.id,
         name: @user.name,
         last_name: @user.last_name,
-        # full_legal_number: @user.legal_number + @user.legal_number_verification,
+        full_legal_number: @user.legal_number + @user.legal_number_verification,
         nickname: @nickname,
         role: General::User.what_role?(@user),
         company: @user.company,
