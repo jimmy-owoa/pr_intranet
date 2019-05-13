ec5bf32d14d02fafa290ea7d61ebf72e9ff730f4
Moisés Donoso Salinas <moisesdonoso@gmail.com>
2019-05-13 11:13:07 -0400

    frontned controller

--- app/controllers/frontend/frontend_controller.rb
+++ app/controllers/frontend/frontend_controller.rb
@@ -1,6 +1,6 @@
 class Frontend::FrontendController < ApplicationController
   #login / Applicationcontroller with base, no api
-  # before_action :authenticate_request
+  before_action :authenticate_request
   attr_reader :current_user
 
   def index
