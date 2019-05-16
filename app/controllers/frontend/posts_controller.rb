a97e7b8f32edddb5aaf9e8fe3bc7d1162054551b
Moisés Donoso Salinas <moisesdonoso@gmail.com>
2019-05-16 17:46:12 -0400

    fix

--- app/controllers/frontend/posts_controller.rb
+++ app/controllers/frontend/posts_controller.rb
@@ -106,6 +106,8 @@ module Frontend
       content = content.gsub("<source src=\"", '<video src="')
       content = content.gsub("<img src=\"../..", '<img src="http://localhost:3000')
       content = content.gsub("<img src=\"rails/", '<img src="http://localhost:3000/rails/')
+      # content = content.gsub("<video style=\"float: right;\"", "<source style=\"float: right;\"")
+
     else
       content = content.gsub("<source src=\"../..", '<video src="http://intranet-security-qa-v1.s3-website.us-east-2.amazonaws.com')
       content = content.gsub("<source src=\"", '<video src="')
