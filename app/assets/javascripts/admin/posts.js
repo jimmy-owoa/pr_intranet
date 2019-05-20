a223251a7df6bb1d65b142d23d47d24a8c7f79bf
Moisés Donoso Salinas <moisesdonoso@gmail.com>
2019-05-20 17:44:09 -0400

    load image select

--- app/assets/javascripts/admin/posts.js
+++ app/assets/javascripts/admin/posts.js
@@ -1,5 +1,19 @@
 $(document).on("turbolinks:load", function() {
-  $("[rel=tooltip]").tooltip({ placement: 'right'});
+  function readURL(input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+      reader.onload = function(e) {
+        $("#image_preview")
+          .attr("src", e.target.result)
+          .width(180)
+          .height(120);
+      };
+
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
+
+  $("[rel=tooltip]").tooltip({ placement: "right" });
   $("#terms_names").select2({
     tags: true,
     tokenSeparators: [",", " "]
