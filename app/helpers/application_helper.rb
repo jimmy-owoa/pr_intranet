fb38f4241a9a48a9cc22238751716cec0f45e8d1
Moisés Donoso Salinas <moisesdonoso@gmail.com>
2019-05-27 10:20:26 -0400

    style

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -139,8 +139,11 @@ module ApplicationHelper
 
   def show_media_index file
     if supported_img(file) && file.attached?
-      return image_tag file.attachment.variant(resize: "360x300>")
-    elsif supported_video(file) && file.attached?
+      # if  file.attachment.metadata.dig("width") <= 450 && file.attachment.metadata.dig("height") <= 450
+        return image_tag file.attachment.variant(resize: "200x250>")
+      # elsif (file.attachment.metadata.dig("width") > 450 && file.attachment.metadata.dig("height") > 450) 
+      #   return image_tag file.attachment.variant(combine_options: { gravity: 'Center', crop: '200x250+0+0' })
+    elsif supported_video(file) && file.attached? 
       return video_tag(url_for(file.attachment), width: '224px')
     else
       return '<h4>Archivo no soportado</h4>'.html_safe
