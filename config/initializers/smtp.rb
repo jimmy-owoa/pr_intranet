9fc994284e84016ed7ff203eed1eb2e63a4ffd2c
Jeremías <jeremias@exaconsultores.cl>
2019-07-08 11:21:47 -0400

    credentials

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   address: 'smtp.sendgrid.net',
   port: 587,
   domain: 'misecurity.elmejorlugarparatrabajar.cl',
-  user_name: Rails.application.credentials.SENDGRID_USERNAME,
-  password: Rails.application.credentials.SENDGRID_PASSWORD,
+  user_name: Rails.application.credentials.sendgrid[:username],
+  password: Rails.application.credentials.sendgrid[:password],
   authentication: :login,
   enable_starttls_auto: true
 }
