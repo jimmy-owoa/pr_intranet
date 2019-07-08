1d393d9240bb26e1fe6f498696cfccd9dfb6945d
Jeremías <jeremias@exaconsultores.cl>
2019-07-08 11:11:23 -0400

    use rails credentials over .env

--- config/application.rb
+++ config/application.rb
@@ -1,13 +1,9 @@
 require_relative 'boot'
-
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-Dotenv::Railtie.load
-HOSTNAME = ENV['HOSTNAME']
-#SENDGRID ENV
 module Intranet
   class Application < Rails::Application
     config.autoload_paths += %W(#{config.root}/services)
--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   address: 'smtp.sendgrid.net',
   port: 587,
   domain: 'misecurity.elmejorlugarparatrabajar.cl',
-  user_name: ENV['SENDGRID_USERNAME'],
-  password: ENV['SENDGRID_PASSWORD'],
+  user_name: Rails.application.credentials.SENDGRID_USERNAME,
+  password: Rails.application.credentials.SENDGRID_PASSWORD,
   authentication: :login,
   enable_starttls_auto: true
 }
