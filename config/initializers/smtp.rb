1ef5ddff16d0971ef096825779e0184650d1fb2a
Jeremías <jeremias@exaconsultores.cl>
2019-07-08 15:24:57 -0400

    smtp config with new variables

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   address: 'smtp.sendgrid.net',
   port: 587,
   domain: 'misecurity.elmejorlugarparatrabajar.cl',
-  user_name: Rails.application.credentials.sendgrid_username,
-  password: Rails.application.credentials.sendgrid_password,
+  user_name: Rails.env.production? ? ENV["SENDGRID_USERNAME"] : Rails.application.credentials.sendgrid_username,
+  password: Rails.env.production? ? ENV["SENDGRID_PASSWORD"] : Rails.application.credentials.sendgrid_password,
   authentication: :login,
   enable_starttls_auto: true
 }
