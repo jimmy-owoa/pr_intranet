92571d97ad5d64956fdb9f74da8a2da0ddb76672
Jeremías <jeremias@exaconsultores.cl>
2019-07-08 11:48:41 -0400

    now trying with real credentials smtp

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   address: 'smtp.sendgrid.net',
   port: 587,
   domain: 'misecurity.elmejorlugarparatrabajar.cl',
-  user_name: "Rails.application.credentials.sendgrid[:username]",
-  password: "Rails.application.credentials.sendgrid[:password]",
+  user_name: Rails.application.credentials.sendgrid[:username],
+  password: Rails.application.credentials.sendgrid[:password],
   authentication: :login,
   enable_starttls_auto: true
 }
