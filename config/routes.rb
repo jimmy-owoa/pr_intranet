0111a01b18671dab7289fa87d6c3763601ddd1d3
Jeremías <jeremias@exaconsultores.cl>
2019-07-22 13:27:12 -0400

    route for exa menu peru

--- config/routes.rb
+++ config/routes.rb
@@ -80,6 +80,7 @@ Rails.application.routes.draw do
     get 'indicators', to: 'frontend#indicators'
     get 'births/calendar_births', to: 'births#calendar_births'
     get 'menus/api_menu/:user_id(/:location_id)', to: 'menus#api_menu'
+    get 'menus/api_menu_vue/:user_id(/:location_id)', to: 'menus#api_menu_vue'
     get 'sso_auth/:token', to: 'users#set_user'
     resources :galleries
     resources :weather
