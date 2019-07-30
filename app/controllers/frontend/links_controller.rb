9facfd6d9b6060cddc5270447047936dee137984
Jeremías <jeremias@exaconsultores.cl>
2019-07-30 16:01:24 -0400

    image last 8

--- app/controllers/frontend/links_controller.rb
+++ app/controllers/frontend/links_controller.rb
@@ -1,13 +1,14 @@
 module Frontend
   class LinksController < FrontendController
     def index
-      links = General::Link.last(5)
+      links = General::Link.last(8)
       data = []
       links.each do |link|
         data << {
           id: link.id,
           title: link.title,
           created_at: l(link.created_at, format: '%A %d %B %Y'),
+          image: link.image.attached? ? url_for(link.image) : root_url + ActionController::Base.helpers.asset_url('noimage.png'),
           url: link.url
         }
       end
