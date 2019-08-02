2dacac2942a9fd4866f87b115911b1cf2994dca3
Jeremías <jeremias@exaconsultores.cl>
2019-08-02 17:46:52 -0400

    benefits charge fix

--- app/services/exa_benefits_service.rb
+++ app/services/exa_benefits_service.rb
@@ -10,8 +10,16 @@ class ExaBenefitsService
     @data["beneficiary_groups"].each do |bg|
       benefit_group = General::BenefitGroup.where(name: bg["name"], code: bg["code"]).first_or_create
       bg["benefits"].each do |benefit|
-        record = General::Benefit.where(title: benefit["name"], code: benefit["code"], benefit_type_id: 1).first_or_create
-        record.benefit_groups << benefit_group unless record.benefit_groups.include?(benefit_group)
+        if /^(\s)*bono/.match(benefit["name"].downcase)
+          benefit_type = 1
+        elsif /^(\s)*crédito/.match(benefit["name"].downcase)
+          benefit_type = 2
+        elsif /^(\s)*seguro/.match(benefit["name"].downcase)
+          benefit_type = 3
+        elsif /^(\s)*permiso/.match(benefit["name"].downcase)
+          benefit_type = 4
+        end
+        record = General::Benefit.where(title: benefit["name"], code: benefit["code"], benefit_type_id: benefit_type).first_or_create
         if benefit["variables"].present?
           benefit["variables"].each do |variable|  
             var = General::BenefitGroupRelationship.where(amount: variable["amount"], currency: variable["currency"], url: benefit["url"], benefit_group: benefit_group).first_or_create
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_18_172317) do
+ActiveRecord::Schema.define(version: 2019_08_02_200039) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -111,7 +111,6 @@ ActiveRecord::Schema.define(version: 2019_07_18_172317) do
     t.integer "benefit_group_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "variable_id"
     t.string "amount"
     t.string "currency"
     t.string "url"
