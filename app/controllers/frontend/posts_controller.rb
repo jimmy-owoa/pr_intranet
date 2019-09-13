5d888cafc7335a2647853262dd7e019b190024cd
Jeremías <jimmyguionbajo@gmail.com>
2019-09-13 13:09:57 -0300

    added 6 to title posts

--- app/controllers/frontend/posts_controller.rb
+++ app/controllers/frontend/posts_controller.rb
@@ -12,7 +12,7 @@ module Frontend
       @image = post.main_image.present? ? url_for(post.main_image.attachment) : root_url + '/assets/news.jpg'
       data << {
         id: post.id,
-        title: post.title.length > 36 ? post.title.slice(0..36) + '...' : post.title,
+        title: post.title.length > 36 ? post.title.slice(0..42) + '...' : post.title,
         main_image: post.main_image,
         published_at: post.created_at.strftime("%d/%m/%Y · %H:%M"),
         post_type: post.post_type.present? ? post.post_type.upcase : '',
