8ae678481bbaa1a22d9005a25854b0dfc4e54cce
Samuel Infante <sinfante@redvel.cl>
2019-09-27 11:13:57 -0300

    set_user de profile

--- app/models/general/profile.rb
+++ app/models/general/profile.rb
@@ -1,23 +1,37 @@
 class General::Profile < ApplicationRecord
-  class Attributes
+  class Classes
     ALL = [
-      POSITION_CLASSIFICATION = "position_classification",
-      IS_BOSS = "is_boss",
-      EMPLOYEE_CLASSIFICATION = "employee_classification",
-      HAS_CHILDREN = "has_children"
+      "gender",
+      "position_classification",
+      "is_boss",
+      "employee_classification",
+      "has_children",
+      "syndicate_member",
+      "contract_type",
+      "rol",
+      "schedule"
     ]
-    GENDER = [
-      MASCULINO = "masculino",
-      FEMENINO = "femenino",
-      GENDER_ALL = "['masculino', 'femenino']"
-    ]
-  end
-
-  Attributes::GENDER.each do |attr|
-    scope attr, ->{ where(value: attr) }
   end
 
   has_many :user_profiles, class_name: 'General::UserProfile', foreign_key: :profile_id, inverse_of: :profile
   has_many :profile_attributes, class_name: 'General::ProfileAttribute', foreign_key: :profile_id, inverse_of: :profile
+  has_many :users, through: :user_profiles
 
+  def set_users
+    query = "1"
+    Classes::ALL.each do |class_name|
+      profile_attributes_where = self.profile_attributes.where(class_name: class_name).map{|v| "'#{v.value}'"}.join(',')
+      query += " AND #{class_name} IN (#{profile_attributes_where})" if profile_attributes_where.present?
+    end
+
+    General::User.where(query).each do |user|
+      self.users << user
+    end
+  end
 end
+
+# "location_regions", # model
+#       "general_benefit_group", # model
+#       "company", # model
+#       "company_management", # model
+#       "company_cost_center", # model
