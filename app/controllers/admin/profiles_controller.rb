c73fce240121d8685363df7a4d17bc3a1ad816ba
Jeremías <jimmyguionbajo@gmail.com>
2019-09-30 17:00:55 -0300

    profiles update

--- app/controllers/admin/profiles_controller.rb
+++ app/controllers/admin/profiles_controller.rb
@@ -24,8 +24,8 @@ module Admin
     end
 
     def users_list
-      @profile = General::Profile.includes(user_profiles: :user ).find(params[:id])
-      render xlsx: 'users_list.xlsx.axlsx', filename: "listado de usuarios #{@profile.name + ' ' + l(Date.today, format: '%A %d %B %Y') }.xlsx"
+      @profile = General::Profile.includes(user_profiles: :user).find(params[:id])
+      render xlsx: "users_list.xlsx.axlsx", filename: "listado de usuarios #{@profile.name + " " + l(Date.today, format: "%A %d %B %Y")}.xlsx"
     end
 
     def new
@@ -34,6 +34,8 @@ module Admin
     end
 
     def edit
+      get_data
+      get_selected
     end
 
     def create
@@ -68,6 +70,20 @@ module Admin
     def update
       respond_to do |format|
         if @profile.update(profile_params)
+          set_class_name_value(params[:regions], "location_region")
+          set_class_name_value(params[:benefit_groups], "general_benefit_group")
+          set_class_name_value(params[:companies], "company")
+          set_class_name_value(params[:managements], "company_management")
+          set_class_name_value(params[:genders], "gender")
+          set_class_name_value(params[:is_boss], "is_boss")
+          set_class_name_value(params[:employee_classifications], "employee_classification")
+          set_class_name_value(params[:cost_centers], "company_cost_center")
+          set_class_name_value(params[:position_classifications], "position_classification")
+          set_class_name_value(params[:syndicate_members], "syndicate_member")
+          set_class_name_value(params[:contract_types], "contract_type")
+          set_class_name_value(params[:roles], "rol")
+          set_class_name_value(params[:schedules], "schedule")
+          set_class_name_value(params[:has_children], "has_children")
           format.html { redirect_to admin_profile_path(@profile), notice: "Profile fue actualizada con éxito." }
           format.json { render :show, status: :ok, location: @profile }
         else
@@ -105,8 +121,32 @@ module Admin
       @has_children = ["si", "no"]
     end
 
+    def get_selected
+      @selected_genders = @profile.profile_attributes.where(class_name: "gender").pluck(:value)
+      @selected_boss = @profile.profile_attributes.where(class_name: "is_boss").pluck(:value)
+      @selected_employee_classifications = @profile.profile_attributes.where(class_name: "employee_classification").pluck(:value)
+      @selected_companies = @profile.profile_attributes.where(class_name: "company").pluck(:value)
+      @selected_regions = @profile.profile_attributes.where(class_name: "location_region").pluck(:value)
+      @selected_benefit_groups = @profile.profile_attributes.where(class_name: "general_benefit_group").pluck(:value)
+      @selected_managements = @profile.profile_attributes.where(class_name: "company_management").pluck(:value)
+      @selected_cost_centers = @profile.profile_attributes.where(class_name: "company_cost_center").pluck(:value)
+      @selected_position_classifications = @profile.profile_attributes.where(class_name: "position_classification").pluck(:value)
+      @selected_syndicate_members = @profile.profile_attributes.where(class_name: "syndicate_member").pluck(:value)
+      @selected_contract_types = @profile.profile_attributes.where(class_name: "contract_type").pluck(:value)
+      @selected_roles = @profile.profile_attributes.where(class_name: "rol").pluck(:value)
+      @selected_schedules = @profile.profile_attributes.where(class_name: "schedule").pluck(:value)
+      @selected_has_children = @profile.profile_attributes.where(class_name: "has_children").pluck(:value)
+    end
+
     def set_class_name_value(values, class_name)
-      if values 
+      if values
+        #Update
+        values.each do |value|
+          General::ProfileAttribute.where(class_name: class_name, profile_id: @profile.id).where.not(value: value).each do |del|
+            del.delete
+          end
+        end
+        #Create
         values.each do |value|
           General::ProfileAttribute.where(class_name: class_name, value: value, profile_id: @profile.id).first_or_create
         end
@@ -120,7 +160,7 @@ module Admin
     def profile_params
       params.require(:profile).permit(:name, regions: [], benefit_groups: [], companies: [], managements: [], genders: [], is_boss: [],
                                              employee_classifications: [], cost_centers: [], position_classifications: [],
-                                             syndicate_members: [], contract_types: [], roles: [], schedules: [], has_childrens: [], 
+                                             syndicate_members: [], contract_types: [], roles: [], schedules: [], has_childrens: [],
                                              entry_dates: [])
     end
   end
