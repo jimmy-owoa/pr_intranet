50b936204963876d8191292a3485a12ce5c0950a
Samuel Infante <sinfante@redvel.cl>
2019-10-03 16:29:36 -0300

    fix for application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,22 +4,19 @@ class ApplicationController < ActionController::Base
   before_action :set_ip
   protect_from_forgery
   before_action :set_locale
+  before_action :auth_user
 
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
   def set_ip
     @ip = Rails.env.production? ? Rails.application.credentials.production : Rails.application.credentials.develop # Develop ip
   end
 
-  def after_sign_in_path_for(user)
-    if user.has_role? :super_admin or user.has_role? :post_admin
-      return admin_root_path
-    else
-      return frontend_welcome_path
-    end
+  def auth_user
+    redirect_to user_azure_oauth2_omniauth_authorize_path if !current_user
   end
 
-  def after_sign_out_path_for(resource_or_scope)
-    new_user_session_path
+  def after_sign_in_path_for(resource)
+    session[:url] || root_path
   end
 
   def set_locale
