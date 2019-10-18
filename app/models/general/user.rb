ab8fcc6a4ec358cb14a9b4d58c749c392bc9d137
Jeremías <jimmyguionbajo@gmail.com>
2019-10-18 15:05:04 -0300

    user expiration admin

--- app/models/general/user.rb
+++ app/models/general/user.rb
@@ -34,7 +34,7 @@ class General::User < ApplicationRecord
 
   accepts_nested_attributes_for :terms
 
-  devise :trackable, :database_authenticatable, :omniauthable, omniauth_providers: [:azure_oauth2]
+  devise :trackable, :timeoutable, :database_authenticatable, :omniauthable, omniauth_providers: [:azure_oauth2]
 
   # callbacks
   after_create :assign_default_role, :image_resize
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,8 +3,8 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
 # Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
-  config.mailer_sender = 'no-reply@example.com'
-  require 'devise/orm/active_record'
+  config.mailer_sender = "no-reply@example.com"
+  require "devise/orm/active_record"
   config.case_insensitive_keys = [:email]
   config.strip_whitespace_keys = [:email]
   config.skip_session_storage = [:http_auth]
@@ -14,6 +14,7 @@ Devise.setup do |config|
   config.password_length = 6..128
   config.email_regexp = /\A[^@\s]+@[^@\s]+\z/
   config.reset_password_within = 6.hours
+  config.timeout_in = 15.minutes
   config.sign_out_via = :delete
   aad_credentials = Rails.application.credentials.aad
   config.omniauth :azure_oauth2, client_id: aad_credentials[:client_id], client_secret: aad_credentials[:client_secret], tenant_id: aad_credentials[:tenant_id]
