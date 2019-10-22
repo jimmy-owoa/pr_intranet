17520aa3f199649574ac9c60ceb43695bc7550bf
Jeremías <jimmyguionbajo@gmail.com>
2019-10-22 17:19:00 -0300

    fix births approve (it was sending email and its not configured yet)"

--- app/controllers/admin/births_controller.rb
+++ app/controllers/admin/births_controller.rb
@@ -1,10 +1,10 @@
-module Admin 
+module Admin
   class BirthsController < AdminController
     before_action :set_birth, only: [:show, :destroy, :edit, :update]
 
     def index
       add_breadcrumb "Nacimientos", :admin_births_path
-      if params[:approved] == 'true' || params[:approved] == 'false'
+      if params[:approved] == "true" || params[:approved] == "false"
         aprov = ActiveModel::Type::Boolean.new.cast(params[:approved])
         @births = Employee::Birth.order(created_at: :desc).approved_filter(aprov).paginate(:page => params[:page], :per_page => 10)
       else
@@ -34,38 +34,38 @@ module Admin
       @birth = Employee::Birth.new(birth_params)
       respond_to do |format|
         if @birth.save
-          format.html { redirect_to admin_birth_path(@birth), notice: 'Nacimiento fue creado con éxito.'}
-          format.json { render :show, status: :created, location: @birth}
+          format.html { redirect_to admin_birth_path(@birth), notice: "Nacimiento fue creado con éxito." }
+          format.json { render :show, status: :created, location: @birth }
         else
-          format.html {render :new}
-          format.json {render json: @birth.errors, status: :unprocessable_entity}
+          format.html { render :new }
+          format.json { render json: @birth.errors, status: :unprocessable_entity }
         end
       end
     end
 
     def update
-      approved = params['approved']
+      approved = params["approved"]
       if approved.present?
         respond_to do |format|
           if approved == "true"
-            UserNotifierMailer.send_birth_approved(@birth.user.email).deliver 
+            # UserNotifierMailer.send_birth_approved(@birth.user.email).deliver
           else
-            UserNotifierMailer.send_birth_not_approved(@birth.user.email).deliver
+            # UserNotifierMailer.send_birth_not_approved(@birth.user.email).deliver
           end
           @birth.update_attributes(approved: approved)
-          format.json { render :json => {value: "success"} and return}
+          format.json { render :json => { value: "success" } and return }
         end
-      elsif params['image_id'].present?
-        ActiveStorage::Attachment.find(params['image_id']).update_attributes(permission: 1)
+      elsif params["image_id"].present?
+        ActiveStorage::Attachment.find(params["image_id"]).update_attributes(permission: 1)
       else
         respond_to do |format|
           if @birth.update(birth_params)
             catch_image(params[:permissions])
-            format.html { redirect_to admin_birth_path(@birth), notice: 'Nacimiento fue actualizado con éxito.'}
+            format.html { redirect_to admin_birth_path(@birth), notice: "Nacimiento fue actualizado con éxito." }
             format.json { render :show, status: :ok, location: @birth }
           else
-            format.html { render :edit}
-            format.json { render json: @birth.errors, status: :unprocessable_entity}
+            format.html { render :edit }
+            format.json { render json: @birth.errors, status: :unprocessable_entity }
           end
         end
       end
@@ -79,7 +79,7 @@ module Admin
       end
     end
 
-   def delete_image
+    def delete_image
       @image = ActiveStorage::Attachment.find(params[:id])
       @image.purge
       redirect_back(fallback_location: root_path)
@@ -87,14 +87,15 @@ module Admin
 
     def destroy
       @birth.destroy
-      UserNotifierMailer.send_birth_not_approved(@birth.user.email).deliver
+      # UserNotifierMailer.send_birth_not_approved(@birth.user.email).deliver
       respond_to do |format|
-        format.html { redirect_to admin_births_path, notice: 'Nacimiento fue eliminado con éxito.'}
+        format.html { redirect_to admin_births_path, notice: "Nacimiento fue eliminado con éxito." }
         format.json { head :no_content }
       end
     end
 
     private
+
     # Use callbacks to share common setup or constraints between actions.
     def set_birth
       @birth = Employee::Birth.find(params[:id])
@@ -102,8 +103,7 @@ module Admin
 
     def birth_params
       params.require(:birth).permit(:full_name_mother, :full_name_father, :child_name, :is_public, :child_lastname,
-        :child_lastname2, :birthday, :approved, :gender, :user_id, images: [])
+                                    :child_lastname2, :birthday, :approved, :gender, :user_id, images: [])
     end
-
   end
 end
