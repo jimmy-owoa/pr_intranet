cd4c08dd464827bd32ec5b9bb3fa87453d215dac
Jeremías <jimmyguionbajo@gmail.com>
2019-10-22 16:10:45 -0300

    devise rollback. my bad

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -14,7 +14,7 @@ Devise.setup do |config|
   config.password_length = 6..128
   config.email_regexp = /\A[^@\s]+@[^@\s]+\z/
   config.reset_password_within = 6.hours
-  config.timeout_in = 4.hours
+  config.timeout_in = 15.minutes
   config.sign_out_via = :delete
   aad_credentials = Rails.application.credentials.aad
   config.omniauth :azure_oauth2, client_id: aad_credentials[:client_id], client_secret: aad_credentials[:client_secret], tenant_id: aad_credentials[:tenant_id]
