ba351cb68146551cb3e2f6141fb226be0fce573e
Javier Colihuinca <javier@exaconsultores.cl>
2019-11-05 13:34:45 -0300

    changes gemfile and database.yml

--- db/schema.rb
+++ db/schema.rb
@@ -159,7 +159,6 @@ ActiveRecord::Schema.define(version: 2019_10_30_140521) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "benefit_group_id"
     t.bigint "benefit_type_id"
     t.string "code"
     t.string "url"
@@ -445,7 +444,6 @@ ActiveRecord::Schema.define(version: 2019_10_30_140521) do
   end
 
   create_table "general_weather_informations", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "location_id"
     t.date "date"
     t.string "max_temp"
     t.string "min_temp"
@@ -466,6 +464,7 @@ ActiveRecord::Schema.define(version: 2019_10_30_140521) do
     t.string "aaa_tomorrow_icon"
     t.string "aaa_tomorrow_max"
     t.string "aaa_tomorrow_min"
+    t.integer "location_id"
     t.integer "uv_index"
   end
 
