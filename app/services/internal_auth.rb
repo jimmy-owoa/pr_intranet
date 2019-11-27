cea0e0fabc4f745d8e1485c6d68c72cc6ef4ec33
Jeremías <jeremias@exaconsultores.cl>
2019-11-27 13:00:35 -0300

    exa service encrypt changed

--- app/services/internal_auth.rb
+++ app/services/internal_auth.rb
@@ -9,29 +9,36 @@ class InternalAuth
     cipher = OpenSSL::Cipher.new "aes-256-cbc"
     cipher.decrypt
     cipher.key = cipher_key || "EB5932580C920015B65B4B308FF7F352"
+    # Proceso de desencriptación
     unescaped = CGI.unescape(data) # Se le quita el urlencode
-    # se encuentran los datos el IV y del dato encriptado separados por &
-    base64_data = unescaped.split("&")
-    # Se descodifica de base64 cada dato
-    decoded_iv = Base64.decode64(base64_data[0])
-    decoded_encrypt = Base64.decode64(base64_data[1])
-    cipher.iv = decoded_iv # Se carga el IV
-    decrypted = cipher.update(decoded_encrypt) # Se hace el primer paso de desencriptación
+    decoded = Base64.decode64(unescaped) # Se descodifica de base64
+    cipher.iv = decoded[0..15] # Se carga el IV. Este corresponde a los primeros 16 caracteres de la data recibida
+    decrypted = cipher.update(decoded[16..decoded.length - 1]) # Se hace el primer paso de desencriptación
     decrypted << cipher.final # Se finaliza la desencriptación
+    # Se considera que esté dentro de 1 minuto la solicitud
     timestamp = decrypted[-10..(decrypted.length - 1)].to_i
-    return decrypted[0..(decrypted.length - 11)]
+    if ((timestamp - 30)..(timestamp + 30)).include?(Time.now.utc.to_i)
+      # Se retorna el dato del usuariodesencriptado
+      return decrypted[0..(decrypted.length - 11)]
+    else
+      puts "Error de coincidencia de timestamp. Hora local: " + Time.now.utc.to_i.to_s + " - timestamp: " + timestamp.to_s
+      raise "Error de coincidencia de timestamp. Hora local: " + Time.now.utc.to_i.to_s + " - timestamp: " + timestamp.to_s
+    end
   end
 
   def self.encrypt(raw_user_cod, cipher_key = nil)
     timestamp = Time.now.utc.to_i
     secret = cipher_key || "EB5932580C920015B65B4B308FF7F352"
-    cipher = OpenSSL::Cipher.new("aes-256-cbc")
+    cipher = OpenSSL::Cipher::Cipher.new("aes-256-cbc")
+
     iv = cipher.random_iv
     cipher.encrypt
     cipher.key = secret
+
     encrypted_data = cipher.update(raw_user_cod + timestamp.to_s)
     encrypted_data << cipher.final
-    return CGI.escape(Base64.strict_encode64(iv.to_s) + "&" + Base64.strict_encode64(encrypted_data))
+
+    return CGI.escape(Base64.strict_encode64(iv.to_s + encrypted_data))
   end
   # en rails hay que llamar al otro servicio
 end
