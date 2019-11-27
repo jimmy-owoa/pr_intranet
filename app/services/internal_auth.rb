a35d91aa7a3af0330c6c602b25b4faac062a0f47
Jeremías <jeremias@exaconsultores.cl>
2019-11-27 11:48:52 -0300

    internal auth service done

--- app/services/internal_auth.rb
+++ app/services/internal_auth.rb
@@ -25,13 +25,13 @@ class InternalAuth
   def self.encrypt(raw_user_cod, cipher_key = nil)
     timestamp = Time.now.utc.to_i
     secret = cipher_key || "EB5932580C920015B65B4B308FF7F352"
-    cipher = OpenSSL::Cipher::Cipher.new("aes-256-cbc")
+    cipher = OpenSSL::Cipher.new("aes-256-cbc")
     iv = cipher.random_iv
     cipher.encrypt
     cipher.key = secret
     encrypted_data = cipher.update(raw_user_cod + timestamp.to_s)
     encrypted_data << cipher.final
-    return CGI.escape(Base64.strict_encode64(iv.to_s) + "&" + Base64.strict_encode64(encrypted_data)) # Se retorna agregando '&' para separar iv con la data en el decrypt
+    return CGI.escape(Base64.strict_encode64(iv.to_s) + "&" + Base64.strict_encode64(encrypted_data))
   end
   # en rails hay que llamar al otro servicio
 end
