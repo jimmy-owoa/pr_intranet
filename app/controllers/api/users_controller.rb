b39ce800a2a8b51c0d88e46ff26f1cf318e30e1d
Jeremías <jeremias@exaconsultores.cl>
2019-11-28 17:47:29 -0300

    api users add

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -19,6 +19,7 @@ module Api
       @user = General::User.new(user_params)
       set_user_data
       if @user.save
+        add_relations
         render json: @user, status: :created
       else
         render json: @user.errors, status: :unprocessable_entity
@@ -42,9 +43,9 @@ module Api
 
       parent = General::User.where(legal_number: params[:parent_legal_number][0...-1], legal_number_verification: params[:parent_legal_number][-1]).first if params[:parent_legal_number].present?
       benefit_group_id = General::BenefitGroup.find_by_name(params[:benefit_group_name]).try(:id)
-      cost_center = Company::CostCenter.where(name: params[:cost_center]).first_or_create
-      management = Company::Management.where(name: params[:management]).first_or_create
-      company = Company::Company.where(name: params[:company]).first_or_create
+      cost_center = Company::CostCenter.where(name: params[:cost_center_name]).first_or_create
+      management = Company::Management.where(name: params[:management_name]).first_or_create
+      company = Company::Company.where(name: params[:company_name]).first_or_create
       @user.company = company
       @user.management = management
       @user.office = office
@@ -54,6 +55,29 @@ module Api
       @user.location_id = @user.try(:office).try(:commune).try(:city_id)
     end
 
+    def add_relations
+      user_education_ids = []
+      JSON.parse(params[:education]).each do |institution|
+        education_state = PersonalData::EducationState.where(name: institution[1]["Estado"]).first_or_create
+        education_institution = PersonalData::EducationState.where(name: institution[1]["Estado"]).first_or_create
+        user_education_ids << PersonalData::UserEducation.where(user_id: @user.id, education_institution_id: education_institution.id, education_state_id: education_state.id).first_or_create.id
+      end
+
+      user_language_ids = []
+      JSON.parse(params[:languages]).each do |lang|
+        level = PersonalData::LanguageLevel.where(level: lang[1]["Nivel"]).first_or_create
+        language = PersonalData::Language.where(name: lang[1]["Idioma"], level_id: level.id).first_or_create
+        user_language_ids << PersonalData::UserLanguage.where(user_id: @user.id, language_id: language.id, level_id: level.id).first_or_create.id
+      end
+      @user.user_language_ids = user_language_ids
+
+      family_member_ids = []
+      JSON.parse(params[:family_group]).each do |member|
+        family_member_ids << PersonalData::FamilyMember.where(user_id: @user.id, relation: member[1]["Relación"], birthdate: member[1]["Fecha nacimiento de familiar"], gender: member[1]["Sexo de familiar"]).first_or_create.id
+      end
+      @user.family_member_ids = family_member_ids
+    end
+
     # def find_user
     #   @user = General::User.find_by_legal_number!(params[:legal_number])
     # rescue ActiveRecord::RecordNotFound
@@ -61,11 +85,11 @@ module Api
     # end
 
     def find_user
-      if params[:encrypted_data].present?
-        legal_number = InternalAuth.decrypt(params[:encrypted_data])
+      if params[:user_code_crypted_base64].present?
+        legal_number = InternalAuth.decrypt(params[:user_code_crypted_base64])
         @user = General::User.find_by_legal_number!(legal_number[0...-1])
       else
-        render json: { errors: "Encrypted data not present" }, status: :error
+        render json: { errors: "User code crypted not present" }, status: :error
       end
     rescue ActiveRecord::RecordNotFound
       render json: { errors: "User not found" }, status: :not_found
