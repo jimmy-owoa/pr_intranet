59c771d7577d5ec1928591580a3178aebfae0c29
Javier Colihuinca <javier@exaconsultores.cl>
2019-12-10 10:43:46 -0300

    FIX. Validate Author, Category and Editorial
    Books. Validate that these attributes are uniques

--- app/models/library/editorial.rb
+++ app/models/library/editorial.rb
@@ -1,5 +1,5 @@
 class Library::Editorial < ApplicationRecord
     has_many :books, class_name: "Library::Book"
 
-    validates :name, presence: true
+    validates :name, presence: true, uniqueness: { case_sensitive: false }
 end
