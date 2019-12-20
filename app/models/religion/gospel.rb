ee220176b4c6b106c8ee25f3d8cd78e2b19b8492
Javier Colihuinca <javier@exaconsultores.cl>
2019-12-20 16:30:55 -0300

    FIX - Avance

--- app/models/religion/gospel.rb
+++ app/models/religion/gospel.rb
@@ -1,2 +1,15 @@
 class Religion::Gospel < ApplicationRecord
+  scope :gospel_today, -> { where(date: Date.today).present? ? Religion::Gospel.where(date: Date.today).last : Religion::Gospel.last }
+
+  scope :gospel_days, -> (days) { where(date: Date.today).present? ? Religion::Gospel.where(date: Date.today).last(days) : Religion::Gospel.last }
+
+  def self.get_gospel(number)
+    if number.positive?
+      gospels = Religion::Gospel.where(date: Date.today + number.days).last
+    elsif number.negative?
+      gospels = Religion::Gospel.where(date: Date.today - number.days).last
+    else
+      gospels = "Sin Datos"
+    end 
+  end
 end
