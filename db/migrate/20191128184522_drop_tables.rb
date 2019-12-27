ede8aede7081411dc39b5c4f859fe7143cc75e7e
Jeremías <jeremias@exaconsultores.cl>
2019-12-27 16:12:36 -0300

    FIX - removed drop tables

--- db/migrate/20191128184522_drop_tables.rb
+++
@@ -1,9 +0,0 @@
-class DropTables < ActiveRecord::Migration[5.2]
-  def change
-    drop_table :personal_data_education_institutions
-    drop_table :personal_data_education_states
-    drop_table :personal_data_family_members
-    drop_table :personal_data_language_levels
-    drop_table :personal_data_languages
-  end
-end
