bd4ed7d5b0bc20bb2aeda889e122a558b5317181
Javier Colihuinca <javier@exaconsultores.cl>
2020-01-24 11:07:08 -0300

    REFACTORING - Helpers Frontend

---
+++ app/helpers/frontend/frontend_helper.rb
@@ -0,0 +1,30 @@
+module Frontend::FrontendHelper
+  def get_frontend_url
+    case Rails.env
+    when "production"
+      "https://mi.security.cl/#"
+    when "staging"
+      "https://miintranet.exaconsultores.cl/#"
+    else
+      "http://localhost:8080/#"
+    end
+  end
+  
+  def get_exa_request
+    exa_urls = ["https://misecurity-qa3.exa.cl/",
+      "https://misecurity-qa2.exa.cl/",
+      "https://misecurity-qa.exa.cl/",
+      "https://misecurity.exa.cl/"]
+    end
+    
+  def get_request_referer
+    exa_urls = get_exa_request << "https://mi.security.cl/"
+    if request.referer.in?(exa_urls)
+      "https://mi.security.cl/"
+    elsif request.referer == "http://localhost:8080/"
+      "http://localhost:8080/"
+    else
+      "https://miintranet.exaconsultores.cl/"
+    end
+  end
+end
