fe44d881b5debfb2973fc4a99b18169769c3af40
Jeremías <jimmyguionbajo@gmail.com>
2020-02-19 13:11:31 -0300

    FEATURE - Added helper for active li tag

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -338,4 +338,12 @@ module ApplicationHelper
       "https://mi.security.cl/"
     end
   end
+
+  def nav_link(link_text, link_path)
+    class_name = current_page?(link_path) ? "active" : ""
+
+    content_tag(:li, :class => class_name) do
+      link_to link_text, link_path
+    end
+  end
 end
