9aafbeab575b2238c6b6419b5f4711babfecb7f3
Jeremías <jimmyguionbajo@gmail.com>
2020-04-07 10:45:31 -0400

    FIX - Ahora se muestran los menus hijos de los principales

--- app/controllers/admin/menus_controller.rb
+++ app/controllers/admin/menus_controller.rb
@@ -70,8 +70,10 @@ module Admin
       @grouped_options = []
       without_parents = General::Menu.where(parent_id: nil).pluck(:id)
       parents.each do |parent_id|
-        menu = General::Menu.find(parent_id)
-        @grouped_options << [menu.title, menu.id]
+        menus = General::Menu.where(parent_id: parent_id)
+        menus.each do |menu|
+          @grouped_options << [menu.title, menu.id]
+        end
       end
       without_parents.each do |wp|
         menu = General::Menu.find(wp)
