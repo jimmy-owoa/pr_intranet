64b2177557d006d01ebc7fc80f954ca134cfd3ad
javierc22 <javier.ec.22@gmail.com>
2020-04-23 17:01:15 -0400

    FIX. Guardar galería formulario noticias

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -110,7 +110,7 @@ module Admin
     end
 
     def set_gallery
-      @gallery = Media::Gallery.find(params[:post][:gallery]) if params[:post][:gallery].present?
+      @gallery = Media::Gallery.find(params[:gallery_id]) if params[:gallery_id].present?
       @post.gallery = @gallery
       @post.reload.gallery
       @post.save
