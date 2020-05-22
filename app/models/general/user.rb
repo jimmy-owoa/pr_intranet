c070b8a5f41b917e0c6a05fb544a6b2b0ba82b2c
Jeremías <jimmyguionbajo@gmail.com>
2020-05-22 14:04:05 -0400

    FIX - Se saca omniauth de devise

--- app/models/general/user.rb
+++ app/models/general/user.rb
@@ -50,7 +50,7 @@ class General::User < ApplicationRecord
 
   accepts_nested_attributes_for :terms
 
-  devise :trackable, :timeoutable, :database_authenticatable, :omniauthable, omniauth_providers: [:azure_oauth2]
+  devise :trackable, :timeoutable, :database_authenticatable
 
   # callbacks
   after_create :assign_default_role, :image_resize
