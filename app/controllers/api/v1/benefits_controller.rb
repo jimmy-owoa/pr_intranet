86f1fadf0fafdca7b3034f85fb2693fb10873a93
Jeremías <jimmyguionbajo@gmail.com>
2020-05-26 12:34:21 -0400

    Se pasaron los controladores a api

--- app/controllers/api/v1/benefits_controller.rb
+++ app/controllers/api/v1/benefits_controller.rb
@@ -1,60 +1,128 @@
-module Api::V1
-  class BenefitsController < ApiController
-    before_action :find_beneficiary_group, except: %i[create index]
-    before_action :validate
+include ActionView::Helpers::NumberHelper
+
+module Frontend
+  class BenefitsController < FrontendController
+    def index
+      data = []
+      user = @request_user
+      if user.benefit_group.present?
+        data = { benefit_types: [] }
+        @benefit_types = General::BenefitType.all
+        @benefit_types.each do |benefit_type|
+          allowed_benefits = benefit_type.benefits.allowed_by_benefit_group(user.benefit_group.try(:id))
+          if allowed_benefits.present?
+            benefit_type_hash = {
+              name: benefit_type.name,
+              benefits: [],
+            }
+            allowed_benefits.each do |benefit|
+              benefit_type_hash[:benefits] << {
+                id: benefit.id,
+                name: benefit.title,
+                content: benefit.content,
+                image: benefit.image.attached? ? url_for(benefit.image) : root_url + ActionController::Base.helpers.asset_url("benefit.jpg"),
+                url: "admin/benefits/" + "#{benefit.id}" + "/edit",
+                link: benefit.url,
+              }
+            end
+            data[:benefit_types] << benefit_type_hash
+          end
+        end
+      end
+      respond_to do |format|
+        format.json { render json: data }
+      end
+    end
+
+    def benefit
+      data = []
+      id = params[:id].present? ? params[:id] : nil
+      benefit = General::Benefit.find(id)
+      benefit_group = @request_user.benefit_group
+      if benefit_group.benefits.include?(benefit)
+        benefit_ids = benefit_group.benefits.order(:benefit_type_id, :id).map { |x| x.id }
+        benefit_index = benefit_ids.index(benefit.id)
+        types = General::BenefitType.where(id: @request_user.benefit_group.benefits.pluck(:benefit_type_id))
+        parents = General::Benefit.where(benefit_type_id: benefit.benefit_type_id)
+        benefit_parents = []
+        parents.each do |parent|
+          if benefit_group.benefits.include?(parent)
+            benefit_parents << parent
+          end
+        end
+        @image = benefit.image.attached? ? url_for(benefit.image.attachment) : root_url + ActionController::Base.helpers.asset_url("benefit.jpg")
+        data << {
+          id: benefit.id,
+          title: benefit.title,
+          url: root_url + "admin/benefit_groups/" + "#{benefit.id}" + "/edit",
+          content: formatted_content(benefit, benefit.benefit_group_relationships.first),
+          image: @image,
+          types: types,
+          parents: benefit_parents,
+          link: benefit.url,
+          benefit_type: benefit.benefit_type.present? ? benefit.benefit_type.name.downcase : "",
+          prev_id: benefit_ids[benefit_index - 1].present? ? benefit_ids[benefit_index - 1] : benefit_ids.first,
+          next_id: benefit_ids[benefit_index + 1].present? ? benefit_ids[benefit_index + 1] : benefit_ids.last,
+          breadcrumbs: [
+            { href: "/", text: "Inicio" },
+            { href: "/beneficios", text: "Beneficios" },
+            { href: "#", text: benefit.title.truncate(30), disabled: true },
+          ],
+        }
+      end
+      respond_to do |format|
+        format.json { render json: data[0] }
+        format.js
+      end
+    end
+
+    def get_benefit_types
+      types = General::BenefitType.where(id: @request_user.benefit_group.benefits.pluck(:benefit_type_id)).pluck(:name)
+      respond_to do |format|
+        format.json { render json: types }
+      end
+    end
 
     def show
-      data = { beneficiary_group: { name: @beneficiary_group.name, code: @beneficiary_group.code, benefits: [] } }
-      @beneficiary_group.benefits.each { |benefit| data[:beneficiary_group][:benefits] << benefit }
-      render json: data, status: :ok
-    end
-
-    def destroy
-      General::BenefitGroup.where(name: params["name"], code: params["code"]).last.delete
-      render json: "Beneficiary group deleted", status: :ok
-    end
-
-    def create
-      benefit_group = General::BenefitGroup.where(name: params["name"], code: params["code"]).first_or_create
-      data = { beneficiary_group: benefit_group, benefits: [] }
-      JSON.parse(params["benefits"]).each do |benefit|
-        if benefit["name"].present?
-          if /^(\s)*bono/.match(benefit["name"].downcase)
-            benefit_type = 1
-          elsif /^(\s)*crédito/.match(benefit["name"].downcase)
-            benefit_type = 2
-          elsif /^(\s)*seguro/.match(benefit["name"].downcase)
-            benefit_type = 3
-          elsif /^(\s)*permiso/.match(benefit["name"].downcase)
-            benefit_type = 4
-          end
-        end
-        record = General::Benefit.where(title: benefit["name"], code: benefit["code"], benefit_type_id: benefit_type).first_or_create
-        if benefit["variables"].present?
-          benefit["variables"].each do |variable|
-            var = General::BenefitGroupRelationship.where(amount: variable["amount"], currency: variable["currency"], url: benefit["url"], benefit_group: benefit_group).first_or_create
-            record.benefit_group_relationships << var unless record.benefit_group_relationships.include?(var)
-          end
-        end
-        data[:benefits] << record
+      respond_to do |format|
+        format.html
+        format.json { render json: @benefit }
+        format.js
       end
-      render json: data, status: :ok
     end
 
     private
 
-    def validate
-      render json: "Unauthorized", status: 200 if InternalAuth.decrypt(request.headers["Authorization"]) != "exa"
+    def formatted_content(benefit, benefit_group_relationship)
+      key = benefit_group_relationship.currency.present? ? currency_type_format(benefit_group_relationship.currency) : ""
+      val = benefit_group_relationship.amount.present? ? number_to_currency(benefit_group_relationship.amount, unit: "", delimiter: ".", precision: 0) : ""
+      replace_variables = {
+        "TIPO": key,
+        "VALOR": val,
+      }
+      content = benefit.content
+      if (content.present? && content.include?("*|TIPO|*") && content.include?("*|VALOR|*"))
+        replace_variables.each do |key, value|
+          content = content.gsub!("*|#{key}|*", value)
+        end
+      end
+      content
     end
 
-    def find_beneficiary_group
-      @beneficiary_group = General::BenefitGroup.where(name: params[:name], code: params[:code]).last
-    rescue ActiveRecord::RecordNotFound
-      render json: { errors: "Beneficiary group not found" }, status: :not_found
+    def currency_type_format(currency)
+      case currency
+      when "days"
+        "días"
+      when "hours"
+        "horas"
+      else
+        currency
+      end
     end
 
-    def benefit_params
-      params.permit(:beneficiary_group)
+    # Use callbacks to share common setup or constraints between actions.
+    def set_benefit
+      @attachment = General::Benefit.find(params[:id])
     end
   end
 end
