4d83842bc192fa1746e67e617be5cf6444d7879f
javierc22 <javier.ec.22@gmail.com>
2020-05-28 17:46:21 -0400

    FEATURE. Nueva api para indicadores económicos

--- app/jobs/economic_indicator_job.rb
+++ app/jobs/economic_indicator_job.rb
@@ -9,14 +9,13 @@ class EconomicIndicatorJob < ApplicationJob
     uf = data[:uf]
     utm = data[:utm]
     ipc = data[:ipc]
-    ipsa = data[:ipsa]
-    ipsa_variation = data[:ipsa_variation]
+
+    puts("******* Creando Indicadores *******")
     General::EconomicIndicator.where(date: today, economic_indicator_type_id: 1, value: dolar).first_or_create
     General::EconomicIndicator.where(date: today, economic_indicator_type_id: 2, value: euro).first_or_create
     General::EconomicIndicator.where(date: today, economic_indicator_type_id: 3, value: uf).first_or_create
     General::EconomicIndicator.where(date: today, economic_indicator_type_id: 4, value: utm).first_or_create
     General::EconomicIndicator.where(date: today, economic_indicator_type_id: 5, value: ipc).first_or_create
-    General::EconomicIndicator.where(date: today, economic_indicator_type_id: 6, value: ipsa).first_or_create
-    General::EconomicIndicator.where(date: today, economic_indicator_type_id: 7, value: ipsa_variation).first_or_create
+    puts("******* Indicadores creados *******")
   end
 end
