4d2f84de86fffe3143aa2189f4871f2a9d8cbe62
Jeremías <jimmyguionbajo@gmail.com>
2020-12-18 16:57:14 -0300

    sample user

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -99,7 +99,7 @@ class Api::V1::ApiController < ApplicationController
   # end
 
   def get_user
-    #@request_user = General::User.all.sample(1).first
-    @request_user = General::User.find(11)
+    @request_user = General::User.all.sample(1).first
+    # @request_user = General::User.find(11)
   end
 end
