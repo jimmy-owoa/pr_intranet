9eb7eb126a78ffbded9f548037676f4e8ab7af21
javierc22 <javier.ec.22@gmail.com>
2021-07-01 14:56:04 -0400

    FIX migrations for Employee::Birth

--- db/migrate/20181012182814_create_employee_births.rb
+++ db/migrate/20181012182814_create_employee_births.rb
@@ -1,10 +1,9 @@
 class CreateEmployeeBirths < ActiveRecord::Migration[5.2]
   def change
     create_table :employee_births do |t|
-      t.string :full_name_mother
-      t.string :full_name_father
       t.string :child_name
       t.string :child_lastname
+      t.string :child_lastname2
       t.boolean :approved, default: false
       t.boolean :gender
       t.date :birthday
