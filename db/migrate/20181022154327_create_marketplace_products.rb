521c9ee4efb891e181fa8b811fb99a73f1f934e0
javierc22 <javier.ec.22@gmail.com>
2021-07-09 17:09:35 -0400

    Fix products migrations

--- db/migrate/20181022154327_create_marketplace_products.rb
+++ db/migrate/20181022154327_create_marketplace_products.rb
@@ -4,12 +4,16 @@ class CreateMarketplaceProducts < ActiveRecord::Migration[5.2]
       t.string :name
       t.text :description
       t.string :product_type
-      t.decimal :price, precision: 15, scale: 2
+      t.string :currency
+      t.integer :price,
       t.string :email
       t.integer :phone
       t.string :location
       t.integer :expiration
       t.boolean :approved, default: false
+      t.boolean :is_expired, default: false
+      t.date :published_date
+      t.integer :user_id
 
       t.timestamps
     end
