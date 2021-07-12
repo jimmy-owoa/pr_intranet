3254be58f2a3cebb3ef03e477aca52cc135488f9
javierc22 <javier.ec.22@gmail.com>
2021-07-12 13:02:05 -0400

    Fix General::Profile

--- app/controllers/admin/profiles_controller.rb
+++ app/controllers/admin/profiles_controller.rb
@@ -1,34 +1,36 @@
+# frozen_string_literal: true
+
 module Admin
   class ProfilesController < AdminController
-    before_action :set_profile, only: [:show, :edit, :update, :destroy]
+    before_action :set_profile, only: %i[show edit update destroy]
 
     def index
       @profiles = General::Profile.order(created_at: :desc)
     end
 
     def show
-      @regions = Location::Region.find(General::ProfileAttribute.where(profile: @profile, class_name: "location_region").pluck(:value))
-      @companies = Company::Company.find(General::ProfileAttribute.where(profile: @profile, class_name: "company").pluck(:value))
-      @benefit_groups = General::BenefitGroup.find(General::ProfileAttribute.where(profile: @profile, class_name: "benefit_group").pluck(:value))
-      @managements = Company::Management.find(General::ProfileAttribute.where(profile: @profile, class_name: "management").pluck(:value))
-      @genders = General::ProfileAttribute.where(profile: @profile, class_name: "gender").pluck(:value)
-      @is_boss = General::ProfileAttribute.where(profile: @profile, class_name: "is_boss").pluck(:value)
-      @employee_classifications = General::ProfileAttribute.where(profile: @profile, class_name: "employee_classification").pluck(:value)
-      @cost_centers = Company::CostCenter.find(General::ProfileAttribute.where(profile: @profile, class_name: "cost_center").pluck(:value))
-      @position_classifications = General::ProfileAttribute.where(profile: @profile, class_name: "position_classification").pluck(:value)
-      @syndicate_members = General::ProfileAttribute.where(profile: @profile, class_name: "syndicate_member").pluck(:value)
-      @contract_types = General::ProfileAttribute.where(profile: @profile, class_name: "contract_type").pluck(:value)
-      @roles = General::ProfileAttribute.where(profile: @profile, class_name: "rol").pluck(:value)
-      @schedules = General::ProfileAttribute.where(profile: @profile, class_name: "schedule").pluck(:value)
-      @has_children = General::ProfileAttribute.where(profile: @profile, class_name: "has_children").pluck(:value)
-      @office_countries = General::ProfileAttribute.where(profile: @profile, class_name: "office_country").pluck(:value)
-      @office_cities = Location::City.find(General::ProfileAttribute.where(profile: @profile, class_name: "office_city").pluck(:value))
-      @office_regions = Location::Region.find(General::ProfileAttribute.where(profile: @profile, class_name: "office_region").pluck(:value))
+      @regions = Location::Region.find(General::ProfileAttribute.where(profile: @profile, class_name: 'location_region').pluck(:value))
+      @companies = Company::Company.where(id: General::ProfileAttribute.where(profile: @profile, class_name: 'company').pluck(:value))
+      @benefit_groups = General::BenefitGroup.find(General::ProfileAttribute.where(profile: @profile, class_name: 'benefit_group').pluck(:value))
+      @managements = Company::Management.find(General::ProfileAttribute.where(profile: @profile, class_name: 'management').pluck(:value))
+      @genders = General::ProfileAttribute.where(profile: @profile, class_name: 'gender').pluck(:value)
+      @is_boss = General::ProfileAttribute.where(profile: @profile, class_name: 'is_boss').pluck(:value)
+      @employee_classifications = General::ProfileAttribute.where(profile: @profile, class_name: 'employee_classification').pluck(:value)
+      @cost_centers = Company::CostCenter.find(General::ProfileAttribute.where(profile: @profile, class_name: 'cost_center').pluck(:value))
+      @position_classifications = General::ProfileAttribute.where(profile: @profile, class_name: 'position_classification').pluck(:value)
+      @syndicate_members = General::ProfileAttribute.where(profile: @profile, class_name: 'syndicate_member').pluck(:value)
+      @contract_types = General::ProfileAttribute.where(profile: @profile, class_name: 'contract_type').pluck(:value)
+      @roles = General::ProfileAttribute.where(profile: @profile, class_name: 'rol').pluck(:value)
+      @schedules = General::ProfileAttribute.where(profile: @profile, class_name: 'schedule').pluck(:value)
+      @has_children = General::ProfileAttribute.where(profile: @profile, class_name: 'has_children').pluck(:value)
+      @office_countries = General::ProfileAttribute.where(profile: @profile, class_name: 'office_country').pluck(:value)
+      @office_cities = Location::City.find(General::ProfileAttribute.where(profile: @profile, class_name: 'office_city').pluck(:value))
+      @office_regions = Location::Region.find(General::ProfileAttribute.where(profile: @profile, class_name: 'office_region').pluck(:value))
     end
 
     def users_list
       @profile = General::Profile.includes(user_profiles: :user).find(params[:id])
-      render xlsx: "users_list.xlsx.axlsx", filename: "listado de usuarios #{@profile.name + " " + l(Date.today, format: "%A %d %B %Y")}.xlsx"
+      render xlsx: 'users_list.xlsx.axlsx', filename: "listado de usuarios #{@profile.name + ' ' + l(Date.today, format: '%A %d %B %Y')}.xlsx"
     end
 
     def new
@@ -43,12 +45,12 @@ module Admin
 
     def create
       @profile = General::Profile.new(profile_params)
-      params[:profile][:file].present? ? @profile.attached = true : @profile.attached = false
-      
+      @profile.attached = (params[:profile][:file].present? ? true : false)
+
       respond_to do |format|
         if @profile.save
           assign_users
-          format.html { redirect_to admin_profile_path(@profile), notice: "Profile fue creada con éxito." }
+          format.html { redirect_to admin_profile_path(@profile), notice: 'Profile fue creada con éxito.' }
           format.json { render :show, status: :created, location: @profile }
           format.js
         else
@@ -61,11 +63,11 @@ module Admin
 
     def update
       respond_to do |format|
+        @profile.attached = params[:creation_option] != 'manual'
         if @profile.update(profile_params)
-          if params[:profile][:file].present? || !@profile.attached
-            assign_users
-          end
-          format.html { redirect_to admin_profile_path(@profile), notice: "Profile fue actualizada con éxito." }
+          assign_users
+          @profile.update_user_messages
+          format.html { redirect_to admin_profile_path(@profile), notice: 'Profile fue actualizada con éxito.' }
           format.json { render :show, status: :ok, location: @profile }
         else
           format.html { render :edit }
@@ -77,7 +79,7 @@ module Admin
     def destroy
       @profile.destroy
       respond_to do |format|
-        format.html { redirect_to admin_profiles_path, notice: "Profile fue eliminada con éxito." }
+        format.html { redirect_to admin_profiles_path, notice: 'Profile fue eliminada con éxito.' }
         format.json { head :no_content }
       end
     end
@@ -88,7 +90,7 @@ module Admin
       ruts = read_xlsx
       if ruts.present?
         @profile.set_users_by_excel ruts
-      else
+      elsif !@profile.attached
         set_profile_attributes
         @profile.set_users
       end
@@ -106,83 +108,79 @@ module Admin
 
     def set_profile_attributes
       # Para agregar un nuevo filtro, hay que también agregarlo en el array "ALL" dentro del modelo profile.
-      set_class_name_value(params[:regions], "region")
-      set_class_name_value(params[:benefit_groups], "benefit_group")
-      set_class_name_value(params[:companies], "company")
-      set_class_name_value(params[:managements], "management")
-      set_class_name_value(params[:genders], "gender")
-      set_class_name_value(params[:is_boss], "is_boss")
-      set_class_name_value(params[:employee_classifications], "employee_classification")
-      set_class_name_value(params[:cost_centers], "cost_center")
-      set_class_name_value(params[:position_classifications], "position_classification")
-      set_class_name_value(params[:syndicate_members], "syndicate_member")
-      set_class_name_value(params[:contract_types], "contract_type")
-      set_class_name_value(params[:roles], "rol")
-      set_class_name_value(params[:schedules], "schedule")
-      set_class_name_value(params[:has_children], "has_children")
-      set_class_name_value(params[:office_countries], "office_country")
-      set_class_name_value(params[:office_cities], "office_city")
-      set_class_name_value(params[:office_regions], "office_region")
+      set_class_name_value(params[:regions], 'region')
+      set_class_name_value(params[:benefit_groups], 'benefit_group')
+      set_class_name_value(params[:companies], 'company')
+      set_class_name_value(params[:managements], 'management')
+      set_class_name_value(params[:genders], 'gender')
+      set_class_name_value(params[:is_boss], 'is_boss')
+      set_class_name_value(params[:employee_classifications], 'employee_classification')
+      set_class_name_value(params[:cost_centers], 'cost_center')
+      set_class_name_value(params[:position_classifications], 'position_classification')
+      set_class_name_value(params[:syndicate_members], 'syndicate_member')
+      set_class_name_value(params[:contract_types], 'contract_type')
+      set_class_name_value(params[:roles], 'rol')
+      set_class_name_value(params[:schedules], 'schedule')
+      set_class_name_value(params[:has_children], 'has_children')
+      set_class_name_value(params[:office_countries], 'office_country')
+      set_class_name_value(params[:office_cities], 'office_city')
+      set_class_name_value(params[:office_regions], 'office_region')
       # set_class_name_value(params[:has_childrens], "has_children")
     end
 
     def get_data
       users = General::User.all
-      @genders = ["Masculino", "Femenino"]
+      @genders = %w[Masculino Femenino]
       @is_boss = { "Si": 1, "No": 0 }
       @employee_classifications = users.pluck(:employee_classification).uniq.reject(&:blank?).sort
-      @regions = Location::Region.pluck(:id, :name).uniq.reject(&:blank?).sort
-      @benefit_groups = General::BenefitGroup.pluck(:id, :name).sort
-      @companies = Company::Company.pluck(:id, :name).sort
-      @managements = Company::Management.pluck(:id, :name).sort
-      @cost_centers = Company::CostCenter.pluck(:id, :name).sort
+      @regions = Location::Region.order(:name).pluck(:id, :name).uniq.reject(&:blank?)
+      @benefit_groups = General::BenefitGroup.order(:name).pluck(:id, :name)
+      @companies = Company::Company.where.not(name: '').order(:name).pluck(:id, :name)
+      @managements = Company::Management.where.not(name: '').order(:name).pluck(:id, :name)
+      @cost_centers = Company::CostCenter.where.not(name: '').order(:name).pluck(:id, :name)
       @position_classifications = users.pluck(:position_classification).uniq.reject(&:blank?).sort
       @syndicate_members = users.pluck(:syndicate_member).uniq.reject(&:blank?).sort
       @contract_types = users.pluck(:contract_type).uniq.reject(&:blank?).sort
       @roles = users.pluck(:rol).uniq.reject(&:blank?).sort
       @schedules = users.pluck(:schedule).uniq.reject(&:blank?).sort
       @has_children = { "Si": 1, "No": 0 }
-      @office_countries = Location::Country.where.not(name: "").order(:name).map { |country| [country.name, country.id] }
-      @office_cities = Location::City.where.not(name: "").order(:name).map { |city| [city.name, city.id] }
-      @office_regions = Location::Region.where.not(name: "").order(:name).map { |region| [region.name, region.id] }
+      @office_countries = Location::Country.where.not(name: '').order(:name).map { |country| [country.name, country.id] }
+      @office_cities = Location::City.where.not(name: '').order(:name).map { |city| [city.name, city.id] }
+      @office_regions = Location::Region.where.not(name: '').order(:name).map { |region| [region.name, region.id] }
     end
 
     def get_selected
-      @selected_genders = @profile.profile_attributes.where(class_name: "gender").pluck(:value)
-      @selected_boss = @profile.profile_attributes.where(class_name: "is_boss").pluck(:value)
-      @selected_employee_classifications = @profile.profile_attributes.where(class_name: "employee_classification").pluck(:value)
-      @selected_companies = @profile.profile_attributes.where(class_name: "company").pluck(:value)
-      @selected_regions = @profile.profile_attributes.where(class_name: "location_region").pluck(:value)
-      @selected_benefit_groups = @profile.profile_attributes.where(class_name: "benefit_group").pluck(:value)
-      @selected_managements = @profile.profile_attributes.where(class_name: "management").pluck(:value)
-      @selected_cost_centers = @profile.profile_attributes.where(class_name: "cost_center").pluck(:value)
-      @selected_position_classifications = @profile.profile_attributes.where(class_name: "position_classification").pluck(:value)
-      @selected_syndicate_members = @profile.profile_attributes.where(class_name: "syndicate_member").pluck(:value)
-      @selected_contract_types = @profile.profile_attributes.where(class_name: "contract_type").pluck(:value)
-      @selected_roles = @profile.profile_attributes.where(class_name: "rol").pluck(:value)
-      @selected_schedules = @profile.profile_attributes.where(class_name: "schedule").pluck(:value)
-      @selected_has_children = @profile.profile_attributes.where(class_name: "has_children").pluck(:value)
-      @selected_office_countries = @profile.profile_attributes.where(class_name: "office_country").pluck(:value)
-      @selected_office_cities = @profile.profile_attributes.where(class_name: "office_city").pluck(:value)
-      @selected_office_regions = @profile.profile_attributes.where(class_name: "office_region").pluck(:value)
+      @selected_genders = @profile.profile_attributes.where(class_name: 'gender').pluck(:value)
+      @selected_boss = @profile.profile_attributes.where(class_name: 'is_boss').pluck(:value)
+      @selected_employee_classifications = @profile.profile_attributes.where(class_name: 'employee_classification').pluck(:value)
+      @selected_companies = @profile.profile_attributes.where(class_name: 'company').pluck(:value)
+      @selected_regions = @profile.profile_attributes.where(class_name: 'location_region').pluck(:value)
+      @selected_benefit_groups = @profile.profile_attributes.where(class_name: 'benefit_group').pluck(:value)
+      @selected_managements = @profile.profile_attributes.where(class_name: 'management').pluck(:value)
+      @selected_cost_centers = @profile.profile_attributes.where(class_name: 'cost_center').pluck(:value)
+      @selected_position_classifications = @profile.profile_attributes.where(class_name: 'position_classification').pluck(:value)
+      @selected_syndicate_members = @profile.profile_attributes.where(class_name: 'syndicate_member').pluck(:value)
+      @selected_contract_types = @profile.profile_attributes.where(class_name: 'contract_type').pluck(:value)
+      @selected_roles = @profile.profile_attributes.where(class_name: 'rol').pluck(:value)
+      @selected_schedules = @profile.profile_attributes.where(class_name: 'schedule').pluck(:value)
+      @selected_has_children = @profile.profile_attributes.where(class_name: 'has_children').pluck(:value)
+      @selected_office_countries = @profile.profile_attributes.where(class_name: 'office_country').pluck(:value)
+      @selected_office_cities = @profile.profile_attributes.where(class_name: 'office_city').pluck(:value)
+      @selected_office_regions = @profile.profile_attributes.where(class_name: 'office_region').pluck(:value)
     end
 
     def set_class_name_value(values, class_name)
       if values
-        #Update
+        # Update
         values.each do |value|
-          General::ProfileAttribute.where(class_name: class_name, profile_id: @profile.id).where.not(value: value).each do |del|
-            del.delete
-          end
+          General::ProfileAttribute.where(class_name: class_name, profile_id: @profile.id).where.not(value: value).each(&:delete)
         end
-        #Create
+        # Create
         values.each do |value|
           General::ProfileAttribute.where(class_name: class_name, value: value, profile_id: @profile.id).first_or_create
         end
       else
-        General::ProfileAttribute.where(class_name: class_name, profile_id: @profile.id).each do |del|
-          del.delete
-        end
+        General::ProfileAttribute.where(class_name: class_name, profile_id: @profile.id).each(&:delete)
       end
     end
 
--- app/models/general/profile.rb
+++ app/models/general/profile.rb
@@ -1,28 +1,31 @@
+# frozen_string_literal: true
+
 class General::Profile < ApplicationRecord
   class Classes
-    ALL = [
-      "benefit_group",
-      "office_city",
-      "is_boss",
-      "cost_center",
-      "management",
-      "company",
-      "position_classification",
-      "employee_classification",
-      "office_region",
-      "gender",
-      "office_country",
-      "has_children",
-    ]
+    ALL = %w[
+      benefit_group
+      office_city
+      is_boss
+      cost_center
+      management
+      company
+      position_classification
+      employee_classification
+      office_region
+      gender
+      office_country
+      has_children
+    ].freeze
   end
 
-  has_many :user_profiles, class_name: "General::UserProfile", foreign_key: :profile_id, inverse_of: :profile, dependent: :destroy
-  has_many :profile_attributes, class_name: "General::ProfileAttribute", foreign_key: :profile_id, inverse_of: :profile
+  has_many :user_profiles, class_name: 'General::UserProfile', foreign_key: :profile_id, inverse_of: :profile, dependent: :destroy
+  has_many :profile_attributes, class_name: 'General::ProfileAttribute', foreign_key: :profile_id, inverse_of: :profile
   has_many :users, through: :user_profiles
-  has_many :posts, class_name: "News::Post", foreign_key: :profile_id, inverse_of: :profile
-  has_many :messages, class_name: "General::Message", foreign_key: :profile_id, inverse_of: :profile
-  has_many :menus, class_name: "General::Menu", foreign_key: :profile_id, inverse_of: :profile
-  has_many :surveys, class_name: "Survey::Survey", foreign_key: :profile_id, inverse_of: :profile
+  has_many :posts, class_name: 'News::Post', foreign_key: :profile_id, inverse_of: :profile
+  has_many :messages, class_name: 'General::Message', foreign_key: :profile_id, inverse_of: :profile
+  has_many :menus, class_name: 'General::Menu', foreign_key: :profile_id, inverse_of: :profile
+  has_many :surveys, class_name: 'Survey::Survey', foreign_key: :profile_id, inverse_of: :profile
+  has_many :links, class_name: 'General::Link', foreign_key: :profile_id, inverse_of: :profile
 
   # def set_users
   #   query = "1"
@@ -54,31 +57,31 @@ class General::Profile < ApplicationRecord
   # recorrer todos los filtros que nos entregara un listado de usuarios
 
   def set_users
-    query = "1"
+    query = '1'
     class_name = nil
     users_ids = General::User.all.pluck(:id)
     Classes::ALL.each do |class_name|
-      if self.profile_attributes.where(class_name: class_name).present?
-        values = self.profile_attributes.where(class_name: class_name).pluck(:value)
+      if profile_attributes.where(class_name: class_name).present?
+        values = profile_attributes.where(class_name: class_name).pluck(:value)
         users_ids = General::UserAttribute.where(user_id: users_ids).where(attribute_name: class_name, value: values).pluck(:user_id).uniq
       end
     end
 
     query_profile_users = General::User.where(id: users_ids)
     if query_profile_users.present?
-      new_users = query_profile_users - self.users
-      remove_users = self.users - query_profile_users
+      new_users = query_profile_users - users
+      remove_users = users - query_profile_users
     end
 
     if new_users.present?
       new_users.each do |user|
-        self.users << user
+        users << user
       end
     end
 
     if remove_users.present?
       remove_users.each do |user|
-        General::UserProfile.where(user_id: user.id, profile_id: self.id).destroy_all
+        General::UserProfile.where(user_id: user.id, profile_id: id).destroy_all
       end
     end
   end
@@ -86,7 +89,9 @@ class General::Profile < ApplicationRecord
   def set_users_by_excel(ruts)
     users = []
     ruts.each do |rut|
-      user = General::User.where(legal_number: rut.to_s[0...-1], legal_number_verification: rut.to_s[-1]).last
+      ln = rut.class ==  String ? rut : rut.to_i.to_s
+      
+      user = General::User.where(legal_number: ln[0...-1], legal_number_verification: ln[-1]).last  
       users << user if user.present?
     end
 
@@ -101,14 +106,48 @@ class General::Profile < ApplicationRecord
 
     if remove_users.present?
       remove_users.each do |user|
-        General::UserProfile.where(user_id: user.id, profile_id: self.id).destroy_all
+        General::UserProfile.where(user_id: user.id, profile_id: id).destroy_all
       end
     end
   end
 
+  def update_user_messages
+    messages = General::Message.where(profile_id: self.id)
+
+    messages.each do |message|
+      message.set_users
+    end
+  end
+
   def set_query(class_name, query)
-    profile_attributes_where = self.profile_attributes.where(class_name: class_name).map { |v| "'#{v.value}'" }.join(",")
-    query + " AND (attribute_name = '#{class_name}' AND value IN (#{profile_attributes_where}))" if profile_attributes_where.present?
+    profile_attributes_where = profile_attributes.where(class_name: class_name).map { |v| "'#{v.value}'" }.join(',')
+    if profile_attributes_where.present?
+      query + " AND (attribute_name = '#{class_name}' AND value IN (#{profile_attributes_where}))"
+    end
+  end
+
+  def self.get_manager_profiles(current_user)
+    profiles = []
+    manager_companies = current_user.manager_companies.pluck(:company_id)
+    
+    all.each do |profile|
+      # profile_companies = profile.users.pluck(:company_id).uniq
+      profile_companies = profile.profile_attributes.where(class_name: "company").pluck(:value).map(&:to_i)
+      
+      if profile_companies.present? && profile_companies.count <= manager_companies.count
+        profiles << profile if (profile_companies - manager_companies).count.zero?    
+      end
+    end
+
+    profiles
+  end
+
+  def self.get_profiles(current_user)
+    if current_user.is_admin?
+      profiles = all.pluck(:name, :id).sort
+    else
+      profiles = get_manager_profiles(current_user).pluck(:name, :id).sort
+    end
   end
 
   # "1 AND (attribute_name = 'gender' AND value IN ('Masculino, femenino')) AND (attribute_name = 'company' AND value IN ('2'))"
