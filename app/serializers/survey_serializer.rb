2e686b7481d640c2640640611204b2aa7ad2d1db
javierc22 <javier.ec.22@gmail.com>
2021-07-15 10:06:51 -0400

    Show breadcrumbs in "/encuestas/:slug"

--- app/serializers/survey_serializer.rb
+++ app/serializers/survey_serializer.rb
@@ -2,9 +2,18 @@ class SurveySerializer < ActiveModel::Serializer
   attributes :id, :name, :slug, :image
 
   has_many :questions, if: -> { is_show? }
+  attribute :breadcrumbs, if: -> { is_show? }
 
   def image
     object.get_image
+  end
+
+  def breadcrumbs
+    [
+      { to: "/", text: "Inicio", disabled: false, exact: true },
+      { to: "/encuestas", text: "Encuestas", disabled: false, exact: true },
+      { to: "", text: object.name.truncate(30), disabled: true },
+    ]
   end
 
   def is_show?
