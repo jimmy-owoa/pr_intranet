0ca750f8a60d87cf42aa362ae816019138c3b320
javierc22 <javier.ec.22@gmail.com>
2021-09-07 18:23:35 -0300

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -56,6 +56,16 @@ namespace :deploy do
     end
   end
 
+  task :check_revision do
+    on roles(:app) do
+      unless `git rev-parse HEAD` == `git rev-parse origin/#{fetch(:branch)}`
+        puts "WARNING: HEAD is not the same as origin/#{fetch(:branch)}"
+        puts 'Run `git push` to sync changes.'
+        exit
+      end
+    end
+  end
+
   after :publishing, 'deploy:restart'
   after :finishing, 'deploy:cleanup'
 end
