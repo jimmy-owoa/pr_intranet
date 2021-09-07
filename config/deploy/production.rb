6ee34d4b1374965998723d3ff2c23c5d48cc827a
javierc22 <javier.ec.22@gmail.com>
2021-09-07 15:41:47 -0300

    fix

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,4 @@
 set :deploy_to, "/home/ubuntu/apps/prod_helpcenter"
 set :branch, "master-compass-helpcenter"
+set :rails_env, 'production'
 server "3.235.8.128", user: "ubuntu", roles: %w{app db web}
