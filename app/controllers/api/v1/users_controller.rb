92181aca3dccb38b53f10ddc8c32a8aa642a44fc
javierc22 <javier.ec.22@gmail.com>
2021-10-13 19:14:45 -0300

    clean users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -20,56 +20,5 @@ module Api::V1
       data = ActiveModelSerializers::SerializableResource.new(request_user, serializer: UserSerializer)
       render json: { user: data }, status: :ok
     end
-
-    def autocomplete_user
-      search = params[:user] #acá va la variable del search
-      data = []
-      # donde se busca, por si tenemos que agregar más que nombre y apellidos
-      variables = [
-        "name",
-        "last_name",
-        "last_name2",
-        "CONCAT(name,' ',last_name)",
-        "CONCAT(name,' ',last_name,' ',last_name2)",
-        "CONCAT(last_name,' ',last_name2)",
-      ]
-      queries = []
-
-      variables.each do |var|
-        queries << "#{var} LIKE '%#{search}%'"
-      end
-
-      query_where = queries.join(" OR ")
-      results = General::User.where(query_where)
-
-      results.each do |result|
-        data << { value: result.id,
-                 text: result.name + " " + result.last_name }
-      end
-
-      respond_to do |format|
-        format.json { render json: data }
-        format.js
-      end
-    end
-
-    def set_user
-      token = params[:token]
-      # START DECRYPT AND VALIDATION
-      result = General::User.decrypt(token)
-      # END DECRYPT
-      cookies.encrypted[:sso_unt] = {
-        value: result,
-        expires: 1.hour,
-      }
-      render json: { data: cookies.encrypted[:sso_unt] }, callback: "*"
-    end
-
-    def upload
-      image = params[:file_img]
-      @request_user.new_image.attach(image)
-
-      render json: { status: "ok" }, status: :ok
-    end
   end
 end
