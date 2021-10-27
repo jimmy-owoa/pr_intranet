575e0acffbaeccbe4badf57c98ee6c73d63e2d3c
javierc22 <javier.ec.22@gmail.com>
2021-10-26 23:28:13 -0300

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,93 +5,67 @@ puts("******* Creando Roles *******")
 Role.find_or_create_by(name: "helpcenter", label_name: "Centro de ayuda")
 Role.find_or_create_by(name: "admin", label_name: "Administrador")
 
-# - - - - - - - - CIUDADES - - - - - - - - - -
-General::Location.find_or_create_by(name: 'Antofagasta')
-General::Location.find_or_create_by(name: 'Santiago')
-General::Location.find_or_create_by(name: 'Copiapo')
-General::Location.find_or_create_by(name: 'La Serena')
-General::Location.find_or_create_by(name: 'Vina del Mar')
-General::Location.find_or_create_by(name: 'Rancagua')
-General::Location.find_or_create_by(name: 'Talca')
-General::Location.find_or_create_by(name: 'Concepcion')
-General::Location.find_or_create_by(name: 'Temuco')
-General::Location.find_or_create_by(name: 'Puerto Montt')
-
-# - - - - - - - - - - USUARIO ADMINISTRADOR - - - - - - - - - -
-puts("******* Creando Usuario admin *******")
+# # - - - - - - - - - - USUARIO ADMINISTRADOR - - - - - - - - - -
+# puts("******* Creando Usuario admin *******")
 if General::User.find_by(email: "admin@exaconsultores.cl").nil?
   user_admin = General::User.create(
-    name: "Admin Exa", 
+    name: "Admin Exa",
+    last_name: "sa",
     email: "admin@exaconsultores.cl", 
     password: "exaConsultores", 
-    password_confirmation: "exaConsultores", 
-    location_id: 2, 
-    legal_number: "1", 
-    legal_number_verification: "7"
+    password_confirmation: "exaConsultores",  
+    legal_number: "1",
+    id_exa: 9999,
+    country_id: 1
   )
 
   user_admin.add_role :admin
 end
 
-puts("* * * * * * * Creando Usuarios * * * * * * *")
-data_users = JSON.parse(File.read("public/demo_users.json"))
+# STEP 1
+data = JSON.parse(File.read("public/compass_users.json")); nil
+count = data.count
+# STEP 2
+def set_email
+  email = "#{((0...8).map { (65 + rand(26)).chr }.join).downcase}@compass.cl"
+  if General::User.find_by(email: email).present?
+    set_email
+  end
+  email
+end
+# STEP 3
+data.each_with_index do |user, i|
+  if user["email"].present?
+    email = user["email"]
+  else
+    email = set_email
+  end
 
-data_users.each do |user|
-  ln_user = user["RUT"]
-  email = user["Email"]
-  name = user["Nombres"]
-  last_name = user["Apellidos"]
-  position_classification = user["clasificacion_puesto"]
-  is_boss = user["es_jefe"] == "Si"
-  rol = user["rol"]
-  employee_classification = user["clasificacion_empleado"]
-  date_entry = user["fecha_de_ingreso_grupo"]
-  office_address = user["direccion_oficina"]
-  office_commune = user["comuna_oficina"]
-  office_city = user["ciudad_oficina"]
-  office_region = user["region_oficina"]
-  office_country = user["pais_oficina"]
-  birthday = user["fecha_de_nacimento"]
-  civil_status = user["estado_civil"]
-  position = user["cargo"]
-  annexed = user["anexo"]
-  gender = user["sexo"]
-  favorite_name = user["nombre_favorito"]
-  id_exa = user["id_exa"]
-  company = Company::Company.where(name: user["empresa"]).first_or_create
-  country = Location::Country.where(name: office_country).first_or_create
-  region = Location::Region.where(name: office_region, country_id: country.id).first_or_create
-  city = Location::City.where(name: office_city, region_id: region.id).first_or_create
-  commune = Location::Commune.where(name: office_commune, city_id: city.id).first_or_create
-  office = Company::Office.where(address: office_address, commune_id: commune.id).first_or_create
-
-  user_data = General::User.find_by(email: email)
-
-  if user_data.nil?
-    General::User.create(
-      legal_number: ln_user[0...-1], 
-      legal_number_verification: ln_user[-1], 
-      email: email, 
-      name: name, 
-      last_name: last_name, 
-      position_classification: position_classification, 
-      is_boss: is_boss, 
-      rol: rol, 
-      employee_classification: employee_classification, 
-      date_entry: date_entry, 
-      birthday: birthday, 
-      civil_status: civil_status, 
-      position: position, 
-      annexed: annexed, 
-      gender: gender, 
-      favorite_name: favorite_name,
-      company_id: company.id,
-      office_id: office.id, 
-      location_id: 2,
-      id_exa: id_exa,
-      password: "redexa2020", 
-      password_confirmation: "redexa2020"
-    )
+  puts("#{set_email}")
+  puts "====== Quedan #{count - i} usuarios por cargar ======="
+  puts "Cargando usuario: #{email}"
+  hash = {}
+  hash[:email] = email
+  hash[:id_exa] = user["id_exa"]
+  hash[:legal_number] = user["legal_number"]
+  hash[:name] = user["name"]
+  hash[:last_name] = user["last_name"]
+  hash[:last_name2] = user["last_name2"]
+  hash[:position] = user["cargo"]
+  hash[:id_exa_boss] = user["id_exa_boss"]
+  office_address = user["office_address"]
+  hash[:country] = Location::Country.where(name: office_address).first_or_create
+  record = General::User.find_by(id_exa: hash[:id_exa])
+  if(record.present?)
+    puts "Usuario a actualizar. ID: #{record.id} - EMAIL: #{record.email}"
+    if record.update(hash)
+      puts "Actualizado correctamente."
+    else
+      puts "Error: #{record.errors}"
+    end
+  else
+    General::User.create!(hash)
+    puts "++Usuario creado. EMAIL: #{hash[:email]}"
   end
 end
 
@@ -101,8 +75,6 @@ General::User.all.each {|u| u.set_user_attributes }
 # - - - - - - - - - - PERFILES - - - - - - - - - -
 puts("******* Creando perfil: General *******")
 General::Profile.find_or_create_by(name: "General")
-
-
 
 # - - - - - - - - - - CATEGORIAS - - - - - - - - - -
 puts("******* Creando Categorias y preguntas centro de ayuda *******")
