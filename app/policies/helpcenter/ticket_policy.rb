48227114ae4748274345cb880c0cd6f8bfe5da63
javierc22 <javier.ec.22@gmail.com>
2021-11-28 20:40:39 -0300

    Fix show ticket

--- app/policies/helpcenter/ticket_policy.rb
+++ app/policies/helpcenter/ticket_policy.rb
@@ -1,5 +1,5 @@
 class Helpcenter::TicketPolicy < ApplicationPolicy
   def show?
-    user.has_role?(record.category.name, record.category) || user.has_role?(:admin)
+    user.has_role?(record.subcategory.category.name, record.subcategory.category) || user.has_role?(:admin)
   end
 end
