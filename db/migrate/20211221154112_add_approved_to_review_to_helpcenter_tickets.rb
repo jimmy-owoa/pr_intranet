25921499b2744e2f3ef16515ad66662900711ac8
Chisthoper Mella <chisthoper96@gmail.com>
2022-01-10 18:21:43 -0300

    migracion approvedtoreview

--- db/migrate/20211221154112_add_approved_to_review_to_helpcenter_tickets.rb
+++ db/migrate/20211221154112_add_approved_to_review_to_helpcenter_tickets.rb
@@ -1,5 +1,5 @@
 class AddApprovedToReviewToHelpcenterTickets < ActiveRecord::Migration[5.2]
   def change
-    add_column :helpcenter_tickets, :aproved_to_review, :datetime, default: -> {'CURRENT_TIMESTAMP'}
+    add_column :helpcenter_tickets, :aproved_to_review, :boolean, default: true 
   end
 end
