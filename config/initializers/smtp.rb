65bb2fa8d87c8c943046b4714fce880ffb0edc19
Jeremias Gutierrez <jeremiasgup@gmail.com>
2022-03-04 13:43:41 -0300

    FIX - smtp config

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   address: "smtp.gmail.com",
   port: 587,
   domain: "gmail.com",
-  user_name: "centrodeayuda@exa.cl",
-  password: "RedExa@CA",
+  user_name: Rails.env.production? ? "centrodeayuda@exa.cl" : "",
+  password:  Rails.env.production? ? "RedExa@CA" : "",
   authentication: :login,
   enable_starttls_auto: true,
 }
