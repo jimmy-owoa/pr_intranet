e41f6adce6b226e218937b3d79de15ab14aeec55
chisthoper <chisthoper96@gmail.com>
2022-11-09 15:25:54 -0300

    fix

--- app/controllers/admin/chat_messages_controller.rb
+++ app/controllers/admin/chat_messages_controller.rb
@@ -4,7 +4,7 @@ module Admin
       @request = ExpenseReport::Request.find(params[:request_id].to_i)
       chat = Chat::Room.where(resource_id: params[:request_id].to_i, resource_type: params[:resource_type]).first_or_create
       respond_to do |format|
-        if chat.messages.create(message: params[:message], user_id: current_user.id, files: params[:files])
+        if chat.messages.create(message: params[:message], user_id: current_user.id, files: params[:files] || [])
           format.html { redirect_to admin_expense_report_request_path(@request), notice: "Mensaje Guardado" }
           format.json { render :show, status: :ok, location: @request }
         else
